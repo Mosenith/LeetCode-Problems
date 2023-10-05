71e6f7a500811ce66582ae7a8cb500010064ab1e
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-05 16:14:40 -0400

    Test Code

---
+++ src/AlgorithmsCode/Main.java
@@ -0,0 +1,22 @@
+package AlgorithmsCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    // List<String> ls = {"ad*dsa", "adfhiaf"}
+    public static void main(String[] args) {
+        List<String> ls = Arrays.asList("a d*dsa","jfasdf-");
+
+
+        for(int i=0; i<ls.size(); i++) {
+            String tmp = ls.get(i);
+            if(tmp.contains("*") || tmp.contains("#")) {
+                System.out.println("True -> " + tmp);
+            }
+        }
+
+
+    }
+}
