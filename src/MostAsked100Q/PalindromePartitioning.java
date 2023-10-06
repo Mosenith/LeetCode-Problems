5f0f124f2e9924b2aeb6d137fe4f6052a56a17d8
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-06 00:11:41 -0400

    update 131: 10-06-2023

--- src/MostAsked100Q/PalindromePartitioning.java
+++ src/MostAsked100Q/PalindromePartitioning.java
@@ -12,8 +12,62 @@ public class PalindromePartitioning {
 
         System.out.println(partition2(s));
     }
+    // ***************** 1st Method ******************
+    // Approach 1: Use a recursive helper function to generate partitions.
+    // In the recursive function:
+    // - Start from the beginning of the string.
+    // - Iterate through all possible partition points.
+    // - Check if the substring is a palindrome.
+    // - If it's a palindrome, add it to the current partition (subList), recurse for the rest of the string.
+    // - If not a palindrome, skip and try the next partition point.
+    // - Backtrack after recursion to explore other possibilities.
+    // Base Case:
+    // When the start index reaches the end of the string,
+    // add the current partition to the result.
+    // Runtime  : 6ms           -> + 97.46%
+    // Memory   : 55.04 MB      -> + 82.00%
+    static List<List<String>> a = new ArrayList<>();
+    static List<String> subList = new ArrayList<>();
+    public static List<List<String>> partition2(String s) {
+        helper(0,s);
 
+        return a;
+    }
 
+    private static void helper(int start, String s) {
+        if(start == s.length()) {
+            System.out.println("Cur SubList = " + subList);
+            a.add(new ArrayList<>(subList));
+            return;
+        }
+
+        for(int i=start; i<s.length(); i++) {
+            String cur = s.substring(start, i + 1);
+            System.out.println("Cur = " + cur);
+            if (isPalindrome(cur)) {
+                subList.add(cur);
+                helper(i + 1, s);
+                subList.remove(subList.size() - 1);
+            }
+            System.out.println("***************");
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        int len = str.length();
+        for (int i = 0, j = len - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Use BackTrack Approach
+    // Runtime  : 6ms           -> + 97.46%
+    // Memory   : 55.43 MB      -> + 49.94%
     private int n;
     private String s;
     private boolean[][] f;
@@ -52,41 +106,5 @@ public class PalindromePartitioning {
             }
         }
     }
-
-    static List<List<String>> a = new ArrayList<>();
-    static List<String> subList = new ArrayList<>();
-    public static List<List<String>> partition2(String s) {
-        helper(0,s);
-
-        return a;
-    }
-
-    private static void helper(int start, String s) {
-        if(start == s.length()) {
-            System.out.println("Cur SubList = " + subList);
-            a.add(new ArrayList<>(subList));
-            return;
-        }
-
-        for(int i=start; i<s.length(); i++) {
-            String cur = s.substring(start, i + 1);
-            System.out.println("Cur = " + cur);
-            if (isPalindrome(cur)) {
-                subList.add(cur);
-                helper(i + 1, s);
-                subList.remove(subList.size() - 1);
-            }
-            System.out.println("***************");
-        }
-    }
-
-    private static boolean isPalindrome(String str) {
-        int len = str.length();
-        for (int i = 0, j = len - 1; i < j; i++, j--) {
-            if (str.charAt(i) != str.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // ***************** End of 2nd Method ******************
 }
