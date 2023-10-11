72209bc1436d1abfb833cc1ba6fc2d38524149a5
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-10 22:26:18 -0400

    update prob 134

--- src/MostAsked100Q/GasStation.java
+++ src/MostAsked100Q/GasStation.java
@@ -1,5 +1,7 @@
 package MostAsked100Q;
 
+import java.util.Arrays;
+
 public class GasStation {
     public static void main(String[] args) {
         // out: 3
@@ -13,9 +15,11 @@ public class GasStation {
 //        int[] cost = {3,4,3};
 
         // out: 4
-        int[] gas = {5,1,2,3,4};
-        int[] cost = {4,4,1,5,1};
+//        int[] gas = {5,1,2,3,4};
+//        int[] cost = {4,4,1,5,1};
 
+        int[] gas = {3,1,1};
+        int[] cost = {1,2,2};
         System.out.println(canCompleteCircuit(gas,cost));
     }
 
@@ -25,43 +29,42 @@ public class GasStation {
     public static int canCompleteCircuit(int[] gas, int[] cost) {
         // find start index -> gas[i] > cost[i]
         for(int i=0; i<gas.length; i++) {
-            System.out.println("check i = " + i);
             if(gas[i] >= cost[i]) {
-                int tmp = helper(i, gas, cost);
-                if(tmp != -1)
-                    return tmp;
-            }
-            System.out.println("*******");
-        }
+                int totalGas = sum(i, gas, cost);
 
+                if(totalGas != -1) {
+                    return i;
+                }
+            }
+        }
         return -1;
     }
 
-    private static int helper(int start, int[] gas, int[] cost) {
-        System.out.println("**** start = " + start + " ****");
+    private static int sum(int start, int[] gas, int[] cost) {
+        int curTank = gas[start];
+        int index = (start + 1 == gas.length) ? 0 : start + 1;
 
-        int index = start + 1;
-        int tank = gas[start];
-        int costToTravel = cost[start];
         while(index != start) {
+            if((index == 0 && curTank < cost[cost.length-1]) ||
+                    index > 0 && curTank < cost[index-1]) {
+                return -1;
+            }
+            if(index == 0) {
+                curTank += (gas[index] - cost[cost.length-1]);
+            } else {
+                curTank += (gas[index] - cost[index-1]);
+            }
+
+            index++;
             if(index == gas.length) {
                 index = 0;
             }
-
-            if(tank < costToTravel) {
-                System.out.println("Tank less than Cost to Travel!");
-                return -1;
-            }
-
-            tank += (gas[index] - costToTravel);
-            costToTravel = cost[index];
-
-            System.out.println("i = " + index + " : "
-                    + gas[index] + " vs " + cost[index]);
-            if(index == start) break;
-            index++;
         }
 
-        return (tank >= costToTravel) ? start : -1;
+        if(index == 0) {
+            return (curTank < cost[cost.length-1]) ? -1 : 1;
+        }
+        return (curTank < cost[index-1] ? -1 : 1);
     }
+    // ***************** End of 1st Method ******************
 }
