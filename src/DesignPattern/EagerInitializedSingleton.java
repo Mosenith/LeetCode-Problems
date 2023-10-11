137633f4eb2c92c962dcdd67644e8134fe897868
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-11 17:09:00 -0400

    Eager Singleton Class

---
+++ src/DesignPattern/EagerInitializedSingleton.java
@@ -0,0 +1,17 @@
+package DesignPattern;
+
+public class EagerInitializedSingleton {
+    // private static final + init immediately
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+    // private constructor to avoid client applications using the constructor
+    private EagerInitializedSingleton() {
+
+    }
+
+    // public static getInstance() to return the variable
+    public static EagerInitializedSingleton getInstance() {
+        return instance;
+    }
+
+}
