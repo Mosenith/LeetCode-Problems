5da38e4f9f88d9cddc3bad0fe122008ed0745e8f
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-11 20:54:50 -0400

    Thread-Safe Singleton

---
+++ src/DesignPattern/ThreadSafeSingleton.java
@@ -0,0 +1,31 @@
+package DesignPattern;
+
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+
+    }
+
+    // make the global access method synchronized
+    // so that only one thread can execute this method at a time
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if(instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+
+        return instance;
+    }
+
+    // Using double-checked locking implementation
+    public static ThreadSafeSingleton getInstanceUsingDoubleLocking() {
+        if(instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if(instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
