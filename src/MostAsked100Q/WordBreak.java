b3e1bc8112af11e4a54f129e2cc0fefd9e95eaff
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-12 02:31:06 -0400

    incomplete prob 139

---
+++ src/MostAsked100Q/WordBreak.java
@@ -0,0 +1,18 @@
+package MostAsked100Q;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WordBreak {
+    public static void main(String[] args) {
+        String s = "applepenapple";
+        List<String> wordDict = Arrays.asList("apple","pen");
+
+//        String s = "catsandog";
+//        List<String> wordDict = Arrays.asList("cats","dog","sand","and","cat");
+
+        // 3,3,3,4,4
+        // 4,3
+        System.out.println(s.length());
+    }
+}
