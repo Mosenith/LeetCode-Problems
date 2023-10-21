90f9b0abb7f0c574406e72f48ca7c6d229a04da0
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-21 01:37:06 -0400

    unfinished sorting algorithm

---
+++ src/AlgorithmsCode/HeapSortAlgorithm.java
@@ -0,0 +1,19 @@
+package AlgorithmsCode;
+
+import java.util.Arrays;
+
+public class MinSortAlgorithm {
+    public static void main(String[] args) {
+        // [-2,1,2,4,8]
+        int[] arr = {2,1,-2,8,4};
+
+        System.out.println(Arrays.toString(args));
+        sortArray(arr);
+        System.out.println(Arrays.toString(args));
+
+    }
+
+    public static void sortArray(int[] arr) {
+        for()
+    }
+}
