49029fa87e897de22e231290a2f6c41ae6fdbf16
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-26 04:12:54 -0400

    prb 152

--- src/MostAsked100Q/MaximumProductSubarray.java
+++ src/MostAsked100Q/MaximumProductSubarray.java
@@ -2,27 +2,44 @@ package MostAsked100Q;
 
 public class MaximumProductSubarray {
     public static void main(String[] args) {
-//        int[] nums = {-2,0,-1}; // 6
-        int[] nums = {-2,3,-4};
-        // -2,2,2,-3
+//        int[] nums = {2,3,-2,4}; // 6
+        int[] nums = {-1,-1,0};
+        int[] nums1 = {-1,0,-3};
+        //-1,-1,0
+        // -2,2,0,2,-3
         System.out.println(maxProduct(nums));
+        System.out.println("*****");
+        System.out.println(maxProduct(nums1));
+
     }
 
     // ***************** 1st Method ******************
     // Approach 1: Keep max, min, ans & update max & min every iteration
-    // max = max(max(min * n[i], max * n[i]), n[i])
+    // Get temp <- max before update max = max(max(min * n[i], max * n[i]), n[i])
+    // Check if max > ans, set ans = max
     // Runtime  : 1ms            -> + 99.68%
     // Memory   : 40.71MB        -> + 89.16%
     public static int maxProduct(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int ans = Integer.MIN_VALUE;
+        int max = nums[0], min = nums[0], ans = nums[0];
 
-        for(int i=0, j=1; j<nums.length; i++,j++) {
-            int product = nums[i] * nums[j];
-            ans = Math.max(ans, nums[i]);
-            ans = Math.max(ans, nums[j]);
-            ans = Math.max(ans, product);
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;  // store the max because before updating min your max will already be updated
+
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+
+            if (max > ans) {
+                ans = max;
+            }
         }
+
         return ans;
     }
+    // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Merge Sort on ListNode
+    // Runtime  : 9ms            -> + 89.64%
+    // Memory   : 56.78MB        -> + 9.29%
+
 }
