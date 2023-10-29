138a0e3fac8bfcbbb89343f4319b1e7ab7be053e
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-28 23:28:02 -0400

    pro 153

--- src/PathSumII.java
+++ src/PathSumII.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class PathSumII {
+import java.util.List;
+
+public class PathSumII {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+//        root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        int target = 22;
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+
+        System.out.println(pathSum(root2,1));
+    }
+
+    public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+    }
 }
