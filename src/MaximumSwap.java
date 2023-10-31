37e57ac974d261adc09f4edce62eab07dc8a9d13
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-30 21:09:19 -0400

    prob 670

--- src/MaximumSwap.java
+++ src/MaximumSwap.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MaximumSwap {
+    public static void main(String[] args) {
+        int num = 2736; // 7236
+        int num2 = 9973; // 9973
+
+        maximumSwap(num);
+        System.out.println("****");
+        maximumSwap(num2);
+    }
+
+    public static int maximumSwap(int num) {
+        List<Integer> listSort = new ArrayList<>();
+        List<Integer> origin = new ArrayList<>();
+
+        while (num > 0) {
+            listSort.add(num%10);
+            num /= 10;
+        }
+
+//        Collections.reverse(listSort);
+        origin = listSort;
+        System.out.println(origin);
+        Collections.sort(listSort, Collections.reverseOrder());
+        System.out.println(listSort);
+
+        for(int i=0; i<origin.size(); i++) {
+
+        }
+
+        return -1;
+    }
 }
