0383e148d68df19880aa52e7db899b22f221bd57
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-31 23:55:57 -0400

    prob 670

--- src/MaximumSwap.java
+++ src/MaximumSwap.java
@@ -6,7 +6,7 @@ public class MaximumSwap {
         int num2 = 9973; // 9973
 
 
-        System.out.println(maximumSwap(num));
+        System.out.println(maximumSwap2(num2));
         System.out.println("****");
 //        maximumSwap(num2);
     }
@@ -56,4 +56,37 @@ public class MaximumSwap {
     }
     // ***************** End of 1st Method ******************
 
+    public static int maximumSwap2(int num) {
+        String numStr = String.valueOf(num);
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<numStr.length(); i++) {
+            map.put(Integer.valueOf(numStr.charAt(i)-'0'), i);
+        }
+
+        System.out.println(numStr);
+        System.out.println(map);
+
+        for(int i=0; i<numStr.length(); i++) {
+            // check from 9 to numStr[i], if there's num bigger than numStr[i]
+            // if so swap
+            for(int j=9; j<numStr.charAt(i)-'0'; j--) {
+                System.out.println("i=" + i + " -> " + numStr.charAt(i));
+                System.out.println("j=" + j);
+                if(map.containsKey(j)) {
+                    System.out.println("Swappp at j=" + j);
+                    char[] numArray = Integer.toString(num).toCharArray();
+                    // swap j to numStr[i] & numStr[i] to j
+                    char tmp = numArray[i];
+                    numArray[i] = (char) (j + '0');
+                    numArray[map.get(j)] = tmp;
+                    // 2736 -> 7236
+                    String ans = String.copyValueOf(numArray);
+                    return Integer.valueOf(ans);
+                }
+            }
+        }
+
+        return num;
+    }
 }
