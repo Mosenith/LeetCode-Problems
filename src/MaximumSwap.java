53835435a63d435143f3f8ab1b16ac7c59a83f48
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-31 22:43:08 -0400

    prob 670

--- src/MaximumSwap.java
+++ src/MaximumSwap.java
@@ -6,50 +6,54 @@ public class MaximumSwap {
         int num2 = 9973; // 9973
 
 
-        System.out.println(maximumSwap(num2));
+        System.out.println(maximumSwap(num));
         System.out.println("****");
 //        maximumSwap(num2);
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Split num to char array
+    // Use buckets to record the last position of digit 0 ~ 9 in this num
+    // Loop through numArray(left to right) & bucket(right to d[i])
+    // Check if the position of this larger digit is behind the current one
+    // If so swap and return
+    // Runtime  : 1ms            -> + 94.94%
+    // Memory   : 40.74MB        -> + 59.52%
     public static int maximumSwap(int num) {
-        if(num <= 10) return num;
+        char[] digits = Integer.toString(num).toCharArray();
 
-        Stack<Integer> stk = new Stack<>();
-        int max = 0;
-
-        while(num > 0) {
-            max = Math.max(max, num % 10);
-            stk.add(num % 10);
-            num /= 10;
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            System.out.println("digits[i] - '0' = " + (digits[i]- '0'));
+            buckets[digits[i] - '0'] = i;
+            System.out.println(Arrays.toString(buckets));
+            System.out.println("**************");
         }
 
-        System.out.println(stk);
-        System.out.println(max);
+        System.out.println("\n**************");
+        System.out.println(Arrays.toString(digits));
+        System.out.println(Arrays.toString(buckets));
+        System.out.println("**************\n");
 
-        int ans = 0;
-        int tmp = 0;
-        if(stk.peek() != max) {
-            tmp = stk.pop();
-            ans *= max;
-        }
 
-        System.out.println("cur ans = " + ans + ", tmp = " + tmp);
-        System.out.println("stack => " + stk + "\n");
-
-//        if(ans == 1 && tmp == 0) return
-        while(!stk.isEmpty()) {
-            int cur = stk.pop();
-            System.out.println("cur element = " + cur);
-            if(cur == max) {
-                ans = (ans * 10) + tmp;
-            } else {
-                ans = (ans * 10) + cur;
+        for(int i=0; i<digits.length; i++) {
+            for(int j=9; j>digits[i]-'0'; j--) {
+                if(buckets[j] > i) {
+                    // swap
+                    char tmp = digits[i];
+                    System.out.println("i=" + i + " => d[i]=" + tmp);
+                    System.out.println("j=" + j + " => buckets[j]=" + buckets[j]);
+                    digits[i] = digits[buckets[j]];
+                    digits[buckets[j]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+                System.out.println("\n**************");
             }
-            System.out.println(ans);
-            System.out.println("***\n");
-        }
-        System.out.println(ans);
+            System.out.println("===============\n");
 
-        return -1;
+        }
+        return num;
     }
+    // ***************** End of 1st Method ******************
+
 }
