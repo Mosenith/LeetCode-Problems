836b898ae6e16270b3568eefbecc24b12265c8b0
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-01 00:19:01 -0400

    prob 670

--- src/MaximumSwap.java
+++ src/MaximumSwap.java
@@ -17,45 +17,40 @@ public class MaximumSwap {
     // Loop through numArray(left to right) & bucket(right to d[i])
     // Check if the position of this larger digit is behind the current one
     // If so swap and return
-    // Runtime  : 1ms            -> + 94.94%
-    // Memory   : 40.74MB        -> + 59.52%
+    // Runtime  : 0ms            -> + 100%
+    // Memory   : 38.83MB        -> + 89.08%
     public static int maximumSwap(int num) {
         char[] digits = Integer.toString(num).toCharArray();
 
         int[] buckets = new int[10];
         for (int i = 0; i < digits.length; i++) {
-            System.out.println("digits[i] - '0' = " + (digits[i]- '0'));
             buckets[digits[i] - '0'] = i;
-            System.out.println(Arrays.toString(buckets));
-            System.out.println("**************");
         }
-
-        System.out.println("\n**************");
-        System.out.println(Arrays.toString(digits));
-        System.out.println(Arrays.toString(buckets));
-        System.out.println("**************\n");
-
 
         for(int i=0; i<digits.length; i++) {
             for(int j=9; j>digits[i]-'0'; j--) {
                 if(buckets[j] > i) {
                     // swap
                     char tmp = digits[i];
-                    System.out.println("i=" + i + " => d[i]=" + tmp);
-                    System.out.println("j=" + j + " => buckets[j]=" + buckets[j]);
                     digits[i] = digits[buckets[j]];
                     digits[buckets[j]] = tmp;
                     return Integer.valueOf(new String(digits));
                 }
-                System.out.println("\n**************");
             }
-            System.out.println("===============\n");
-
         }
         return num;
     }
     // ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Convert num to String and use Map - key = numDigit, val = digitIndex
+    // Outer Loop - i=0 to numStr.len, Inner Loop - j=9 to numStr[i]
+    // If map containsKey(j) && i<map.get(j) => bigger digit on the right of ith index
+    // Swap by converting num to charArray. Then, convert charArray to String
+    // Finally return Integer of String
+    // Otherwise, return num => no bigger digits on the right
+    // Runtime  : 1ms            -> + 39.27%
+    // Memory   : 39.00MB        -> + 79.19%
     public static int maximumSwap2(int num) {
         String numStr = String.valueOf(num);
         Map<Integer,Integer> map = new HashMap<>();
@@ -64,29 +59,26 @@ public class MaximumSwap {
             map.put(Integer.valueOf(numStr.charAt(i)-'0'), i);
         }
 
-        System.out.println(numStr);
-        System.out.println(map);
-
         for(int i=0; i<numStr.length(); i++) {
             // check from 9 to numStr[i], if there's num bigger than numStr[i]
             // if so swap
-            for(int j=9; j<numStr.charAt(i)-'0'; j--) {
-                System.out.println("i=" + i + " -> " + numStr.charAt(i));
-                System.out.println("j=" + j);
-                if(map.containsKey(j)) {
-                    System.out.println("Swappp at j=" + j);
+            for(int j=9; j>numStr.charAt(i)-'0'; j--) {
+                if(map.containsKey(j) && i < map.get(j)) {
                     char[] numArray = Integer.toString(num).toCharArray();
+
                     // swap j to numStr[i] & numStr[i] to j
                     char tmp = numArray[i];
                     numArray[i] = (char) (j + '0');
                     numArray[map.get(j)] = tmp;
-                    // 2736 -> 7236
+
                     String ans = String.copyValueOf(numArray);
                     return Integer.valueOf(ans);
+                    // Or return Integer.valueOf(new String(numArray));
                 }
             }
         }
-
         return num;
     }
+    // ***************** End of 2nd Method ******************
+
 }
