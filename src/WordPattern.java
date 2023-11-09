f1e7e864d4766b0bcc5386cee6d7a05ac501802d
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-08 19:57:08 -0500

    prob 290

--- src/WordPattern.java
+++ src/WordPattern.java
@@ -12,11 +12,21 @@ public class WordPattern {
         System.out.println(wordPattern(pattern,s));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Use 2 Maps to store char => String && String => char
+    // Use Queue to store all words in s split by space
+    // If map not contain pattern.at(i) = c -> check if rMap.contain(str) && rMap(str) != c, return false
+    // If map not contain pattern.at(i) = c -> !map.get(c).equals(queue.peek()) => return false
+    // Otherwise pop queue and move on
+    // Runtime  : 1ms            -> + 83.64%
+    // Memory   : 40.25MB        -> + 65.17%
     public static boolean wordPattern(String pattern, String s) {
         Map<Character,String> map = new HashMap<>();
         Map<String,Character> rMap = new HashMap<>();
         Queue<String> queue = new ArrayDeque<>();
 
+        // Could use this ==> String[] words = s.split(" ") && add to queue
+        // But take more space
         for(int i=0; i<s.length(); ) {
             int count = i;
             while(count < s.length() && s.charAt(count) != ' ') {
@@ -49,4 +59,7 @@ public class WordPattern {
 
         return true;
     }
+    // ***************** End of 1st Method ******************
+
+
 }
