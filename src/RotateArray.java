4dd196570c56a01a5d186fcfea3e962287fb4b7e
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-12 22:33:05 -0500

    prob 189

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class RotateArray {
@@ -6,7 +7,7 @@ public class RotateArray {
         int[] nums = {1,2};
         int k = 3;
 
-        rotate(nums, k);
+        rotate2(nums, k);
     }
 
     // ***************** 1st Method ******************
@@ -38,6 +39,57 @@ public class RotateArray {
     }
     // ***************** End of 1st Method ******************
 
+    public static void rotate2(int[] nums, int k) {
+        int left = 0, right = nums.length-1;
+
+        while(k > 0) {
+            right--;
+            k--;
+            if(right < left) right = nums.length-1;
+        }
+
+        System.out.println(left + " : " + right);
+        right++;
+        // right+1 then swap with left
+        while(right < nums.length) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            right++;
+            left++;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+    // 1,2,3,4,5,6,7 - 1-5
+
+    public static void rotate3(int[] nums, int k) {
+        if (nums == null || nums.length==0 || k < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        if(k > nums.length){
+            k = k % nums.length;
+        }
+
+        //length of first part
+        int a = nums.length - k;
+        reverse(nums, 0, a-1);
+        reverse(nums, a, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+    }
+
+    public static void reverse(int[] arr, int left, int right) {
+        if(arr == null || arr.length == 1)
+            return;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
 }
 
 
