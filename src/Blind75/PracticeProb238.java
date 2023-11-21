35ba5f3221c4593fc8f786c45c8c010080b8abba
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-21 01:00:03 -0500

    Practice Prob 238

--- src/Blind75/PracticeProb238.java
+++ src/Blind75/PracticeProb238.java
@@ -1,11 +1,42 @@
 package Blind75;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PracticeProb238 {
     public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4};
+        int[] nums2 = {-1,1,0,-3,3};
 
+        System.out.println(Arrays.toString(productExceptSelf(nums1)));
     }
 
-    public int[] productExceptSelf(int[] nums) {
-        return null;
+    public static int[] productExceptSelf(int[] nums) {
+        List<Integer> zeroIndex = new ArrayList<>();
+        int totalProduct = 1;
+        int[] answer = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0) {
+                zeroIndex.add(i);
+            } else {
+                totalProduct *= nums[i];
+            }
+        }
+
+        if(zeroIndex.size() >= 2) {
+            Arrays.fill(answer,0);
+        } else if(zeroIndex.size() == 1) {
+            // all zero except ZeroIndex
+            Arrays.fill(answer,0);
+            answer[zeroIndex.get(0)] = totalProduct;
+        } else {
+            for(int i=0; i<answer.length; i++) {
+                answer[i] = totalProduct / nums[i];
+            }
+        }
+
+        return answer;
     }
 }
