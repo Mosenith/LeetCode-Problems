a541c6f1f88b100ce0321c2b0f86b0d76d9408d9
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-25 01:58:36 -0600

    Prob 338

--- src/CountingBits.java
+++ src/CountingBits.java
@@ -1,15 +1,30 @@
+import java.util.Arrays;
+
 public class CountingBits {
     public static void main(String[] args) {
-        System.out.println(countBits(3));
+        System.out.println(Arrays.toString(countBits(5)));
     }
     public static int[] countBits(int n) {
         int[] arr = new int[n+1];
 
         for(int i=0; i<=n; i++) {
             String b = Integer.toBinaryString(i);
-            System.out.println(b);
+            arr[i] = countOne(b);
+            System.out.println(b + " : " + arr[i]);
         }
 
         return arr;
+    }
+
+    private static int countOne(String b) {
+        if(b.equals("0")) return 0;
+
+        int zero = 0;
+        for(int i=0; i<b.length(); i++) {
+            if(b.charAt(i) == '1')
+                zero++;
+        }
+
+        return zero;
     }
 }
