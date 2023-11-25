af8565e50d9934a30d2b585685038d74170d834e
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-24 22:49:59 -0600

    Practice Prob 191

--- src/Blind75/Binary/PracticeProb191.java
+++ src/Blind75/Binary/PracticeProb191.java
@@ -1,15 +1,13 @@
 package Blind75.Binary;
 
-public class PracticeProb191 {
-    public static void main(String[] args) {
-        String  n = "11111111111111111111111111111101";
-    }
+import java.math.BigInteger;
 
+public class PracticeProb191 {
     public static int hammingWeight(int n) {
         int count = 0;
 
         for(int i=0; i<32 && n!=0; i++) {
-            if((n&10) == 1) {
+            if((n&1) == 1) {
                 count++;
             }
 
