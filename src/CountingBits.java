6a0f31ecf18e338153866bfad4c15dd18bf09cbd
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-26 23:23:52 -0600

    Prob 338: 11-26-2023

--- src/CountingBits.java
+++ src/CountingBits.java
@@ -2,9 +2,34 @@ import java.util.Arrays;
 
 public class CountingBits {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(countBits2(5)));
+        System.out.println(Arrays.toString(countBits2(6)));
     }
+
+    // ***************** 1st Method ******************
+    // Approach : 2 cases - Odd & Even
+    // Odd Num => #1 in bits = a[num/2] + 1
+    // Even Num => #1 in bits = a[num/2]
+    // Runtime  : 2ms           -> + 83.81%
+    // Memory   : 46.45 MB      -> + 39.55%
     public static int[] countBits(int n) {
+        // [0, 1, 1, 2, 1, 2, 2, 3]
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                ans[i] = ans[i / 2];
+            } else {
+                ans[i] = ans[i / 2] + 1;
+            }
+        }
+        return ans;
+    }
+    // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach : Pretty straight forward approach
+    // Runtime  : 10 ms          -> + 12.13%
+    // Memory   : 48.73 MB     -> + 5.95%
+    public static int[] countBits2(int n) {
         int[] arr = new int[n+1];
 
         for(int i=0; i<=n; i++) {
@@ -18,26 +43,13 @@ public class CountingBits {
 
     private static int countOne(String b) {
         if(b.equals("0")) return 0;
-
         int zero = 0;
         for(int i=0; i<b.length(); i++) {
             if(b.charAt(i) == '1')
                 zero++;
         }
-
         return zero;
     }
+    // ***************** End of 2nd Method ******************
 
-    public static int[] countBits2(int n) {
-        // [0, 1, 1, 2, 1, 2, 2, 3]
-        int[] ans = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 == 0) {
-                ans[i] = ans[i / 2];
-            } else {
-                ans[i] = ans[i / 2] + 1;
-            }
-        }
-        return ans;
-    }
 }
