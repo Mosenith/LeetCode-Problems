65c7a1e767ec8c684f9b3595c11885c0b53c24ac
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-11-28 22:58:38 -0600

    Practice Prob 70

--- src/Blind75/DP/PracticeProb70.java
+++ src/Blind75/DP/PracticeProb70.java
@@ -4,7 +4,7 @@ public class PracticeProb70 {
     public static void main(String[] args) {
         int n = 3;
 
-        System.out.println(climbStairs(3));
+        System.out.println(climbStairs2(3));
     }
 
     // recursive
@@ -14,8 +14,23 @@ public class PracticeProb70 {
         // step = 1 or 2
         if(n == 0 || n == 1)
             return 1;
-
-
         return climbStairs(n-1) + climbStairs(n-2);
     }
+
+    public static int climbStairs2(int n) {
+        int a = 0, b = 1;
+
+        for(int i=0; i<n; i++) {
+            int tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+
+        return b;
+    }
 }
+// 0 - 0
+// 1 - 1
+// 2 - 11, 2 = 2
+// 3 - 3
+// 4 - 1111,22,211,112,121
