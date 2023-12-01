1d734b9af82ab150cca8be1c743e94cf7385de4d
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-01 16:28:55 -0600

    Prob 119

--- src/PascalTriangleII.java
+++ src/PascalTriangleII.java
@@ -2,24 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangleII {
-    public List<Integer> getRow(int rowIndex) {
+    public static void main(String[] args) {
+        System.out.println(getRow(3));
+    }
+
+    // ***************** 1st Method ******************
+    // Approach : Use double List and calculate inner based on the prev List
+    // Runtime  : 1ms           -> + 67.19%
+    // Memory   : 40.45 MB      -> + 15.28%
+    public static List<Integer> getRow(int rowIndex) {
         List<List<Integer>> res = new ArrayList<>();
         // 1, 11, 121, 1331, 14641
         // 11 - i=1 - 0,1,2
         // 121 - i=2 -
-        for(int i=0; i<=rowIndex; i++) {
+        res.add(List.of(1));
+
+        for(int i=1; i<=rowIndex; i++) {
             List<Integer> inner = new ArrayList<>();
-
-            for(int j=0; j<i+1; j++) {
-                if(j == 0 || j == i) {
-                    // 1st & last element => 1
-                    inner.add(1);
-                } else {
-
-                }
+            List<Integer> prev = res.get(i-1);
+            inner.add(1);
+            for(int j=0; j<prev.size()-1; j++) {
+                inner.add(prev.get(j) + prev.get(j+1));
             }
+            inner.add(1);
+            res.add(inner);
         }
-
-        return res.get(res.size()-1);
+        return res.get(rowIndex);
     }
+    // ***************** End of 1st Method ******************
+
 }
