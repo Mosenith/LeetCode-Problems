7e1051df8bee174f311b9b3fdf0c46336964e3be
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-04 22:37:40 -0600

    DP Prob 198

--- src/Blind75/DP/HouseRobber.java
+++ src/Blind75/DP/HouseRobber.java
@@ -5,47 +5,25 @@ import java.util.Arrays;
 public class HouseRobber {
     public static void main(String[] args) {
         int[] nums = {2,7,9,3,1};
-        int[] nums2 = {2,1,3};
+        int[] nums2 = {4,1,2,7,5,3,1};
+        int[] nums3 = {2,1,1,2};
 
-        System.out.println(rob(nums2));
+        System.out.println(rob(nums));
     }
 
-    // skip 1, 2, 3 < len-2
-    // start 0, 1,
+    // ***************** 1st Method ******************
+    // Approach : Dynamic Programming
+    // Runtime  : 1ms        -> + 100.00%
+    // Memory   : 39.77MB    -> + 61.89%
     public static int rob(int[] nums) {
-        if(nums.length <= 2) {
-            return Arrays.stream(nums).max().getAsInt();
-        }
-
         int[] dp = new int[nums.length+1];
+        dp[1] = nums[0];
 
-        helper(nums,dp,0,0,1);
-
-        System.out.println(Arrays.toString(dp));
-        int max = Arrays.stream(dp).max().getAsInt();
-        System.out.println(max);
-
-        return max;
-    }
-
-    private static void helper(int[] nums, int[] dp, int index, int start, int skip) {
-        System.out.println(index + " : " + start);
-
-        if(start >= nums.length-2) {
-            if(skip < nums.length-2) {
-                start = 0;
-                skip += 1;
-            } else {
-                return;
-            }
+        for(int i=2; i<=nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
         }
 
-        for(int i=start; i<nums.length && index < dp.length; i+=skip+1) {
-            dp[index] += nums[i];
-        }
-
-        System.out.println(Arrays.toString(dp));
-        helper(nums,dp,index+1, start+1,skip);
-
+        return dp[nums.length];
     }
+
 }
