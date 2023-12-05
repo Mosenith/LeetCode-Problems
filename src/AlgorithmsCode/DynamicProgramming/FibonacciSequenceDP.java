f8a588c0ea5f7f364a69f77544e811388fda423c
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-04 22:20:09 -0600

    Update DP Fib

--- src/AlgorithmsCode/DynamicProgramming/FibonacciSequenceDP.java
+++ src/AlgorithmsCode/DynamicProgramming/FibonacciSequenceDP.java
@@ -11,19 +11,14 @@ public class FibonacciSequenceDP {
     }
 
     private static int fib(int n, Integer[] lookup) {
-        System.out.println("n = " + n);
         if(n == 0 || n == 1) {
             lookup[n] = n;
-            System.out.println(Arrays.toString(lookup));
-            System.out.println("***************** n = " + n + " *******************");
             return lookup[n] = n;
         }
 
         if(lookup[n] == null) {
             lookup[n] = fib(n-1, lookup) + fib(n-2, lookup);
-            System.out.println("lookup = " + Arrays.toString(lookup));
         }
-        System.out.println("***************** n = " + n + " *******************");
         return lookup[n];
     }
 
