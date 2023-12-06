59e588d617978d1d6aa3a67ca9af777fb4b47765
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-05 23:13:47 -0600

    Prob 57

---
+++ src/Blind75/Interval/PracticeProb57.java
@@ -0,0 +1,12 @@
+package Blind75.Interval;
+
+public class PracticeProb57 {
+    public static void main(String[] args) {
+        // [[1,5],[6,9]]
+        int[][] intervals = {{1,3}, {6,9}};
+        int[] newInterval = {2,5};
+    }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        return null;
+    }
+}
