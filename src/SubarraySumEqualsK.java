7952893f96ac80898f9caafde5def650dfb7a1d2
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-05 23:51:38 -0600

    Prob 560

---
+++ src/SubarraySumEqualsK.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        int k = 3;
+
+        System.out.println(subarraySum(nums, k));
+    }
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        int count = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            int curKey = k - nums[i];
+            if(!map.containsKey(nums[i])) {
+                map.put(curKey,i);
+                count++;
+            } else {
+                break;
+            }
+
+            System.out.println(map);
+        }
+
+        return count+1;
+    }
+}
