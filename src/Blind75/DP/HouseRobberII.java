fc01d44298f0e8445599b90be8adaccaa9c91f21
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-10 18:55:05 -0600

    Prob 213

--- src/Blind75/DP/HouseRobberII.java
+++ src/Blind75/DP/HouseRobberII.java
@@ -2,12 +2,30 @@ package Blind75.DP;
 
 public class HouseRobberII {
     public static void main(String[] args) {
-        int[] nums = {2,3,2};   // 3
+        int[] nums = {1,2,3,1,2};   // 3
+        // 1,2,3,1
+
+        System.out.println(rob(nums));
     }
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length+1];
+    public static int rob(int[] nums) {
+        int len = nums.length;
+        if(len == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, len - 2), rob(nums, 1, len - 1));
+    }
 
+    private static int rob(int[] nums, int left, int right) {
+        System.out.println(left + " : " + right);
+        int cur = 0, prev = 0;
 
-        return 0;
+        for(; left<=right; left++) {
+            int tmp = Math.max(cur,prev);
+            cur = prev + nums[left];
+            prev = tmp;
+            System.out.println(cur + " * " + prev);
+        }
+        System.out.println("-----\n");
+        return Math.max(cur,prev);
     }
 }
