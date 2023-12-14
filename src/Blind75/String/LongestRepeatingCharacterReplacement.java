bbbef706cce14fb2d24b41d0c999db592a996e39
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-14 15:12:02 -0600

    Prob 424

---
+++ src/Blind75/String/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,11 @@
+package Blind75.String;
+
+public class LongestRepeatingCharacterReplacement {
+    public static void main(String[] args) {
+
+    }
+
+    public int characterReplacement(String s, int k) {
+        return 0;
+    }
+}
