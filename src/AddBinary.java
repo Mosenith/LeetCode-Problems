0f3fe562598bb5097669bb69268ac7f6c6df5d2c
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-16 01:21:30 -0600

    Prob 67

---
+++ src/AddBinary.java
@@ -0,0 +1,10 @@
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "1010", b = "1011";  // 10101
+//        String a = "11", b = "1";   // 100
+    }
+
+    public String addBinary(String a, String b) {
+        return "";
+    }
+}
