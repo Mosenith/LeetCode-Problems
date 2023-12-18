140fe886b4786b5cee6cf001552bd8b623e180cb
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-17 19:36:32 -0600

    Prob 83

---
+++ src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicatesFromSortedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+}
