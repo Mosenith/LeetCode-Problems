3ef15e89396720f1c44b90b4523d582579619efa
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-17 21:42:04 -0600

    Prob 83

--- src/RemoveDuplicatesFromSortedList.java
+++ src/RemoveDuplicatesFromSortedList.java
@@ -9,11 +9,53 @@ public class RemoveDuplicatesFromSortedList {
         }
     }
 
-    public static void main(String[] args) {
-
+    public static void printList(ListNode node)
+    {
+        while (node != null)
+        {
+            System.out.print(node.val + " —> ");
+            node = node.next;
+        }
+        System.out.print("null\n");
     }
 
-    public ListNode deleteDuplicates(ListNode head) {
-        return null;
+    public static void main(String[] args) {
+        // head = [1,1,2,3,3] -> [1,2,3]
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+
+        System.out.println(deleteDuplicates(head));
+    }
+
+    // ***************** 1st Method ******************
+    // Approach 1:
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 43.32MB     -> + 31.29%
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode pre = null, next = null;
+        ListNode cur = head;
+
+        while(cur != null) {
+            pre = cur;
+            next = cur.next;
+            while(next != null && next.val == pre.val) {
+                next = next.next;
+            }
+
+            cur.next = next;
+            cur = next;
+
+            printList(cur);
+            System.out.println("====\n");
+        }
+
+        printList(head);
+        printList(pre);
+
+        return head;
     }
 }
