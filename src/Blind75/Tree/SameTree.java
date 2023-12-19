051ddbdb9b31575489fe8de482298f71eaad491b
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-19 16:30:35 -0600

    Prob 100

---
+++ src/Blind75/Tree/SameTree.java
@@ -0,0 +1,32 @@
+package Blind75.Tree;
+
+public class SameTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void printTreeNode(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        printTreeNode(root.left);
+        printTreeNode(root.right);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return true;
+    }
+}
