e7ef5682d6f1dcf3d3c1207414078ffc1b746f05
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-21 23:46:34 -0600

    Prob 111

--- src/Blind75/Tree/MinimumDepthOfBinaryTree.java
+++ src/Blind75/Tree/MinimumDepthOfBinaryTree.java
@@ -25,11 +25,40 @@ public class MinimumDepthOfBinaryTree {
     public static void main(String[] args) {
         // 0,1,2
         TreeNode root1 = new TreeNode(2);
-        root1.left = new TreeNode(1);
+//        root1.left = new TreeNode(1);
         root1.right = new TreeNode(4);
+        root1.right.right = new TreeNode(5);
+        root1.right.right.right = new TreeNode(6);
+        root1.right.right.right.right = new TreeNode(7);
+
 //        root1.right.left = new TreeNode(3);
 //        root1.right.right = new TreeNode(6);
 //        root1.right.right.left = new TreeNode(5);
 
+//        printTreeNode(root1);
+        System.out.println(minDepth(root1));
     }
+
+
+    // ***************** 1st Method ******************
+    // Approach 1: check if root is null => 0
+    // Then if root.left = null => 1 + minDepth(root.right)
+    // Then if root.right = null => 1 + minDepth(root.left)
+    // Otherwise => 1 + Math(minDepth(root.left),minDepth(root.right))
+    // Runtime  : 5ms         -> + 36.76%
+    // Memory   : 63.32MB     -> + 7.13%
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+    // ***************** End of 1st Method ******************
+
 }
