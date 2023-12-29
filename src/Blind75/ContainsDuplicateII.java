9f6f6df733eb74a4d3973971df0bea8bfbb4b701
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-28 23:29:15 -0600

    Prob 219

--- src/Blind75/ContainsDuplicateII.java
+++ src/Blind75/ContainsDuplicateII.java
@@ -13,13 +13,15 @@ public class ContainsDuplicateII {
         System.out.println(containsNearbyDuplicate(nums,k));
     }
     // ***************** 1st Method ******************
-    // Approach 1:
+    // Approach 1: Nested loop & inner loop from i+1 to i+k
+    // Within inner loop, check if n[i] == n[j] => true
+    // Otherwise, finally return false
     // Runtime  : 1249ms        -> + 5.02%
     // Memory   : 54.68MB       -> + 91.78%
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<=i+k && j<nums.length; j++) {
-                if(nums[i] == nums[j] && Math.abs(j-i) <= k) {
+                if(nums[i] == nums[j]) {
                     return true;
                 }
             }
