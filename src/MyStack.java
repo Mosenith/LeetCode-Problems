30b51b147a286da3f23573ea8b762a31f5a742b2
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-31 11:06:09 -0600

    Prob 225 base

---
+++ src/MyStack.java
@@ -0,0 +1,21 @@
+public class MyStack {
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
