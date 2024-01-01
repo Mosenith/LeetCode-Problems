b1ff4eab22f6c92327dfe7ba17cf2fa4b298fef3
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-12-31 20:15:06 -0600

    Prob 225 base

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,21 +1,26 @@
-public class MyStack {
-    public MyStack() {
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
 
+public class MyStack {
+    private Stack<Integer> stack;
+    public MyStack() {
+        stack = new Stack<>();
     }
 
     public void push(int x) {
-
+        stack.add(x);
     }
 
     public int pop() {
-
+        return stack.pop();
     }
 
     public int top() {
-
+        return stack.peek();
     }
 
     public boolean empty() {
-
+        return stack.isEmpty();
     }
 }
