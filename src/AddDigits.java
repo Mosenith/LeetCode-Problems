c985231deeac91ff79f45ca5ce60cb9bbc2e815b
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-05 00:17:33 -0600

    Prob 258

--- src/AddDigits.java
+++ src/AddDigits.java
@@ -5,7 +5,7 @@ public class AddDigits {
         System.out.println(addDigits(38));
     }
     // ***************** 1st Method ******************
-    // Approach 1:
+    // Approach 1: Nested Loop
     // Runtime  : 1ms         -> + 85.02%
     // Memory   : 40.82MB     -> + 13.36%
     public static int addDigits(int num) {
@@ -21,9 +21,15 @@ public class AddDigits {
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Consider when num = 0, num%9==0, and num%9!=0
+    // Runtime  : 0ms         -> + 100.0%
+    // Memory   : 41.31MB     -> + 5.94%%
     public static int addDigits2(int num) {
         if(num == 0) return 0;
         else if(num % 9 == 0) return 9;
         else return num % 9;
     }
+    //  ***************** End of 2nd Method ******************
+
 }
