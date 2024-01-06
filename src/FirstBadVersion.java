a779395764c8eb6b51fe48e43abe2c5eb8a57012
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-05 21:48:25 -0600

    Prob 278

--- src/FirstBadVersion.java
+++ src/FirstBadVersion.java
@@ -1,8 +1,6 @@
 public class FirstBadVersion {
-    public static void main(String[] args) {
-        System.out.println(firstBadVersion(5));
-    }
-    public static boolean isBadVersion(int k) {
+    // No main() cos prob has to extends other class in real problem
+    public boolean isBadVersion(int k) {
         if(k == 4) return false;
 
         return true;
@@ -10,13 +8,13 @@ public class FirstBadVersion {
 
     // ***************** 1st Method ******************
     // Approach 1: Using Merge Sort approach to find bad value
-    // Runtime  : 0ms         -> + 100.00%
-    // Memory   : 40.46MB     -> + 24.53%
-    public static int firstBadVersion(int n) {
+    // Runtime  : 24ms         -> + 39.35%
+    // Memory   : 40.69MB     -> + 39.35%
+    public int firstBadVersion(int n) {
         int left = 1, right = n;
 
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; // Or mid = (left + right) >>> 1;
 
             if (isBadVersion(mid)) {
                 right = mid;
