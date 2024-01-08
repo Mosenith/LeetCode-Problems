08d477cd09d7d7d97fafd0aba853ba9d823b906b
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-07 21:45:45 -0600

    Prob 342: 1st Method

--- src/PowerOfFour.java
+++ src/PowerOfFour.java
@@ -3,6 +3,13 @@ public class PowerOfFour {
 
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Base condition if n<=0 => false
+    // Loop with each iteration check if n % 4 != 0 => false
+    // Loop until n == 1 and each iteration n /= 4
+    // Out of loop -> true
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 40.46MB     -> + 24.53%
     public boolean isPowerOfFour(int n) {
         if(n == Integer.MIN_VALUE) return false;
 
@@ -14,4 +21,6 @@ public class PowerOfFour {
 
         return true;
     }
+    //  ***************** End of 1st Method ******************
+
 }
