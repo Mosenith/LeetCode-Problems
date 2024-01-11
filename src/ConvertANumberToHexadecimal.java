fc7b2a727fbae9eb5ed2ea07d307b339cf8b676c
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-11 15:37:24 -0600

    Prob 405

---
+++ src/ConvertANumberToHexadecimal.java
@@ -0,0 +1,30 @@
+public class ConvertANumberToHexadecimal {
+    public static void main(String[] args) {
+        // 26 -> "1a"
+        System.out.println(toHex(26));
+    }
+
+    // ***************** 1st Method ******************
+    // Approach 1: Use & 15 to find remainder and num >>>= 4 to shift 4 digit
+    // return reverse stringbuilder to string
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 41.30MB     -> + 6.56%
+    public static String toHex(int num) {
+        // a-10, b-11, c-12, d-13, e-14, f-15
+        if (num == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            int x = num & 15;
+            if (x < 10) {
+                sb.append(x);
+            } else {
+                sb.append((char) (x - 10 + 'a'));
+            }
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+    //  ***************** End of 1st Method ******************
+}
