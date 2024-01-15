cdadbb3c72f5bef49186f40f9c2b8bedb7ae6ff2
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-15 13:06:05 -0600

    Prob 448: 1st Approach

--- src/FindAllNumbersDisappearedInAnArray.java
+++ src/FindAllNumbersDisappearedInAnArray.java
@@ -9,27 +9,34 @@ public class FindAllNumbersDisappearedInAnArray {
         System.out.println(findDisappearedNumbers(nums));
     }
 
+    // ***************** 3rd Method ******************
+    // Approach 3: Using s.split(" ") and loop through the array of it
+    // When !"".equals(curString), increment the answer
+    // Runtime  : 20ms        -> + 13.19%
+    // Memory   : 53.67MB     -> + 55.93%
     public static List<Integer> findDisappearedNumbers(int[] nums) {
         Arrays.sort(nums);
         List<Integer> ls = new ArrayList<>();
+        int i=1, j=0;
 
-        System.out.println(Arrays.toString(nums));
-        for(int i=1; i<nums.length; i++) {
-            int prev = nums[i-1];
-            int cur = nums[i];
-
-            if(cur != prev && cur-prev != 1) {
-                System.out.println(prev + " : " + cur);
-                while(prev < cur-1){
-                    ls.add(++prev);
-                }
+        for(; i<=nums.length && j<nums.length;) {
+            if(j>0 && nums[j] == nums[j-1]) {
+                j++;
+                continue;
             }
+            if(i != nums[j]){
+                ls.add(i);
+            } else {
+                j++;
+            }
+            i++;
         }
 
-        if(ls.isEmpty()) {
-            ls.add(nums[nums.length-1]);
+        while(i <= nums.length) {
+            ls.add(i++);
         }
-
         return ls;
     }
+    //  ***************** End of 1st Method ******************
+
 }
