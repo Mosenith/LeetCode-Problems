88f276ccd768e007f7af76af95c6bfb1ec450730
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-14 22:42:21 -0600

    Prob 434: 2nd Approach

--- src/NumberOfSegmentsInAString.java
+++ src/NumberOfSegmentsInAString.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberOfSegmentsInAString {
     public static void main(String[] args) {
-        System.out.println(countSegments("                "));
+        System.out.println(countSegments2("Hello"));
     }
 
     // ***************** 1st Method ******************
@@ -24,4 +25,32 @@ public class NumberOfSegmentsInAString {
         return splitString.length;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Have a list store all words separated by whiltespaces & loop through each char in s
+    // Inner loop, increment until index is at whitespace & add s.substring(prev,++i) to list
+    // At i=s.len, add s.substring(prev,i) and otherwise i++
+    // Out of loop, return list.size
+    // Runtime  : 0ms       -> + 100.00%
+    // Memory   : 41.25MB    -> + 21.88%
+    public static int countSegments2(String s) {
+        List<String> ls = new ArrayList<>();
+
+        for(int i=0; i<s.length();) {
+            int left = i;
+            while(i<s.length() && s.charAt(i) != ' ') {
+                i++;
+            }
+            if(i<s.length() && i != left) {
+                ls.add(s.substring(left,++i));
+            } else if(i == s.length()) {
+                ls.add(s.substring(left, i));
+            } else {
+                i++;
+            }
+        }
+
+        return ls.size();
+    }
+    //  ***************** End of 2nd Method ******************
 }
