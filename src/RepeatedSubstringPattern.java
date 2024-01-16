b0f772c2787c4ff09138e24164b1d443ece27c54
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-16 01:57:19 -0600

    Prob 459

--- src/RepeatedSubstringPattern.java
+++ src/RepeatedSubstringPattern.java
@@ -1,2 +1,31 @@
 public class RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        String s = "babbabbabbabbab";
+
+        System.out.println(s.length());
+        System.out.println(repeatedSubstringPattern(s));
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+
+        int mid = s.length()/2;
+        while (mid > 1) {
+            String curSub = s.substring(0,mid);
+            int len = curSub.length();
+            int preMid = mid;
+            while(preMid + len <= s.length()) {
+                if(!curSub.equals(s.substring(preMid,preMid+len))) {
+                    break;
+                }
+
+                if(preMid + len == s.length()) {
+                    return true;
+                }
+                preMid += len;
+            }
+            mid--;
+        }
+
+        return false;
+    }
 }
