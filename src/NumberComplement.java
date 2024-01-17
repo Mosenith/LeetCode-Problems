506c89e49e63961c5ee6b480b9249bcc9d1da83e
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-17 09:48:01 -0600

    Prob 476: 2nd Approach

--- src/NumberComplement.java
+++ src/NumberComplement.java
@@ -1,6 +1,6 @@
 public class NumberComplement {
     public static void main(String[] args) {
-        System.out.println(findComplement(2));
+        System.out.println(findComplement2(2));
     }
 
     // ***************** 1st Method ******************
@@ -20,4 +20,19 @@ public class NumberComplement {
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Set time=0, ans=0, loop while num > 0 & have ans += ((num&1)^1)<<time++
+    // Then shift num >>= 1 and return ans after loop
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 40.42MB     -> + 22.92%
+    public static int findComplement2(int num) {
+        int ans = 0;
+        int time = 0;
+        while (num > 0){
+            ans += ((num&1)^1)<<time++;
+            num >>= 1;
+        }
+        return ans;
+    }
+    //  ***************** End of 2nd Method ******************
 }
