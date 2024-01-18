2c2634db08ff3ddb3afd2b061629c345d59d08d8
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-18 11:52:52 -0600

    Prob 492

---
+++ src/ConstructTheRectangle.java
@@ -0,0 +1,10 @@
+public class ConstructTheRectangle {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] constructRectangle(int area) {
+        int[] side = new int[2];
+
+    }
+}
