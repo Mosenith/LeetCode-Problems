1472140a169a2f3f901cbec8f1fc1194670af899
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-18 11:26:43 -0600

    Prob 491

--- src/NonDecreasingSubsequences.java
+++ src/NonDecreasingSubsequences.java
@@ -9,30 +9,41 @@ public class NonDecreasingSubsequences {
         System.out.println(k.findSubsequences(nums));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Use Recursion & backtracking while set initial curLast=-1000
+    // In dfs(), base case is index>nums.len & inside check if inner.size>1 => add inner to ans
+    // Check nums[index] >= curLast => add to inner & dfs with update index+1 & curLast -> nums[index]
+    // After that recursion backtrack by removing the last element of the inner
+    // Another if check nums[index] != curLast (means smaller) -> dfs with update index+1
+    // Runtime  : 4ms         -> + 96.17%
+    // Memory   : 51.05MB     -> + 43.93%
     List<Integer> inner;
     List<List<Integer>> ans;
     public List<List<Integer>> findSubsequences(int[] nums) {
         inner = new ArrayList<>();
         ans = new ArrayList<>();
 
-        dfs(nums,0);
+        // -1000 is curLast to compare with start
+        dfs(nums,0, -1000);
         return ans;
     }
 
-    private void dfs(int[] nums, int start) {
-        if(start >= nums.length) {
-//            ans.add(new ArrayList<>(inner));
-            inner = new ArrayList<>();
+    private void dfs(int[] nums, int index, int curLast) {
+        if(index >= nums.length) {
+            if(inner.size() > 1)
+                ans.add(new ArrayList<>(inner));
             return;
         }
 
-        if(inner.size() == 0 || inner.size() > 0 && inner.get(inner.size()-1) <= nums[start]) {
-            inner.add(nums[start++]);
-            if(inner.size() > 1) {
-                ans.add(new ArrayList<>(inner));
-            }
+        if(nums[index] >= curLast) {
+            inner.add(nums[index]);
+            dfs(nums, index+1, nums[index]); // recursion
+            inner.remove(inner.size()-1);   // backtrack
         }
 
-        dfs(nums, start);
+        if(nums[index] != curLast) {
+            dfs(nums, index+1, curLast);
+        }
     }
+    //  ***************** End of 1st Method ******************
 }
