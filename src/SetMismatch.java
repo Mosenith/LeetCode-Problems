37f8bcdbb825fc858d6a2bb11ed140b45975d101
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-22 17:00:30 -0600

    Prob 645: 2nd & 3rd Approach

--- src/SetMismatch.java
+++ src/SetMismatch.java
@@ -4,7 +4,7 @@ public class SetMismatch {
     public static void main(String[] args) {
         int[] nums = {3,2,3,4,6,5};
 
-        System.out.println(Arrays.toString(findErrorNums(nums)));
+        System.out.println(Arrays.toString(findErrorNums2(nums)));
     }
 
     // ***************** 1st Method ******************
@@ -35,4 +35,53 @@ public class SetMismatch {
         return ans.stream().mapToInt(Integer::intValue).toArray();
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Use sum of arithmetics numbers - s,s1,s2
+    // s-All sum in nums, s1-Well organized arithmetics nums(1-n), s2-Sums in nums without dup elements
+    // return array of {s - s2,s1 - s2} with s-s2 => Get repeated number & s2-s1 => find missing number
+    // Runtime  : 0ms       -> + 100.00%
+    // Memory   : 41.25MB    -> + 21.88%
+    public static int[] findErrorNums2(int[] nums) {
+        int n = nums.length;
+        int s1 = (1 + n) * n / 2;
+        int s2 = 0;
+        Set<Integer> set = new HashSet<>();
+        int s = 0;
+
+        for (int x : nums) {
+            if (set.add(x)) {
+                s2 += x;
+            }
+            s += x;
+        }
+        // s2-s1 => find missing number
+        // s-s2 => Get repeated number
+        return new int[] {s - s2, s1 - s2};
+    }
+    //  ***************** End of 2nd Method ******************
+
+    // ***************** 3rd Method ******************
+    // Approach 3: Similar to 1st method - since we know there are 2 elements in the return array
+    // Have an occ[] to track n in nums, so if occ[i]=0 -> missing num
+    // If occ[i]=2 -> duplicated num
+    // Runtime  : 2ms         -> + 97.67%
+    // Memory   : 46.28MB     -> + 5.04%
+    public static int[] findErrorNums3(int[] nums) {
+        int[] occurrences = new int[nums.length];
+
+        for(int num : nums){
+            occurrences[num-1]++;
+        }
+
+        int[] solution = new int[2];
+
+        for(int i = 0; i < occurrences.length; i++){
+            if(occurrences[i] == 0) solution[1] = i+1;
+            if(occurrences[i] == 2) solution[0] = i+1;
+        }
+
+        return solution;
+    }
+    //  ***************** End of 3rd Method ******************
 }
