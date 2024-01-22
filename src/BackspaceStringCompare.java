f51a48320b4721a9863252fdaf3eb7cd30d53388
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-22 01:49:53 -0600

    Prob 844

---
+++ src/BackspaceStringCompare.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class BackspaceStringCompare {
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("ab#c","ad#c"));
+    }
+
+    public static boolean backspaceCompare(String s, String t) {
+        Stack<Character> stk1 = new Stack<>();
+        Stack<Character> stk2 = new Stack<>();
+
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+
+        util(sChar, stk1);
+        util(tChar, stk2);
+
+        if(stk1.size() != stk2.size()) return false;
+
+        while(!stk1.isEmpty()) {
+            if(stk1.pop() != stk2.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void util(char[] chars, Stack<Character> stk) {
+        for(char c : chars) {
+            if(c == '#') {
+                if(!stk.isEmpty()) {
+                    stk.pop();
+                }
+            } else {
+                stk.add(c);
+            }
+        }
+    }
+
+}
