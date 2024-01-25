f0277982821dde0a9f9f38eef2685a853df49a45
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-25 01:46:46 -0600

    Prob 1143: update comment

--- src/LongestCommonSubsequence.java
+++ src/LongestCommonSubsequence.java
@@ -10,6 +10,10 @@ public class LongestCommonSubsequence {
 
     // ***************** 1st Method ******************
     // Approach 1: Use DP storing memoize in f[t1.len+1][t2.len+1]
+    // If characters match, take the value from the diagonal cell and increment by 1
+    // f[i][j] = f[i - 1][j - 1] + 1;
+    // If characters do not match, take the maximum of the cell above and the cell to the left
+    // f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
     // Runtime  : 20ms         -> + 82.44%
     // Memory   : 50.92MB      -> + 26.88%
     public static int longestCommonSubsequence(String text1, String text2) {
@@ -17,14 +21,16 @@ public class LongestCommonSubsequence {
         int[][] f = new int[m + 1][n + 1];
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
+                // If characters match, take the value from the diagonal cell and increment by 1
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    System.out.println("Equal at i-" + i + ", j-" + j);
                     f[i][j] = f[i - 1][j - 1] + 1;
                 } else {
+                    // If characters do not match, take the maximum of the cell above and the cell to the left
                     f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
                 }
             }
         }
+        // The value in the bottom-right cell represents the length of the longest common subsequence
         return f[m][n];
     }
     //  ***************** End of 1st Method ******************
