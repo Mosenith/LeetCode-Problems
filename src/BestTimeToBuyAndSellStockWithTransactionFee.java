aa75292685edc3535f96831718af592d77cc99bb
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-29 22:46:10 -0600

    Prob 714

--- src/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,9 +1,20 @@
 public class BestTimeToBuyAndSellStockWithTransactionFee {
     public static void main(String[] args) {
-
+        int[] prices = {1,3,2,8,4,9};
+        int fee = 2;
     }
 
+    int[] prices;
+    int fee;
     public int maxProfit(int[] prices, int fee) {
+        this.prices = prices;
+        this.fee = fee;
 
+        return dp(0,false);
+    }
+
+    public int dp(int pos, boolean buy) {
+        if(pos == prices.length)
+            return 0;
     }
 }
