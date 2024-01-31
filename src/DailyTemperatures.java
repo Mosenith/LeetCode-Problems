3200dbd17677c8a41dfec66b1c620cb10f2b0bda
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-30 21:46:34 -0600

    Prob 739

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+
 public class DailyTemperatures {
     public static void main(String[] args) {
-        int[] temperatures = {73,74,75,71,69,72,76,73};
+        int[] temperatures = {30,60,90};
+
+        DailyTemperatures dt = new DailyTemperatures();
+        System.out.println(Arrays.toString(dt.dailyTemperatures3(temperatures)));
     }
-    public int[] dailyTemperatures(int[] temperatures) {
+
+    // brute force : work but over time limit
+    public int[] dailyTemperatures3(int[] temperatures) {
         int len = temperatures.length;
         int[] ans = new int[len];
 
+        for(int i=0; i<len-1; i++) {
+            int j = i+1;
+            while(j<len && temperatures[i] >= temperatures[j]) {
+                j++;
+            }
+
+            if(j >= len || temperatures[i] > temperatures[j]) {
+                ans[i] = 0;
+            } else {
+                ans[i] = (j-i);
+            }
+        }
 
         return ans;
     }
