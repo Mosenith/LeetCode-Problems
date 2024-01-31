17ed1b180c992321a8f465f6106be0213fd42b53
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-30 19:27:11 -0600

    Prob 739

---
+++ src/DailyTemperatures.java
@@ -0,0 +1,12 @@
+public class DailyTemperatures {
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+    }
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] ans = new int[len];
+
+
+        return ans;
+    }
+}
