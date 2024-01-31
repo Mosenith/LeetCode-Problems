8f24105dfc805f034ed1d2cc9f33e60dabd6b0d8
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-31 00:59:42 -0600

    Prob 792: Base

---
+++ src/NumberOfMatchingSubsequences.java
@@ -0,0 +1,9 @@
+public class NumberOfMatchingSubsequences {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String[] words = {"a","bb","acd","ace"};
+    }
+    public int numMatchingSubseq(String s, String[] words) {
+        return 0;
+    }
+}
