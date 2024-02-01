50f0ed0668b187b5ebd735d782763d5f53924dd0
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-01 01:29:32 -0600

    Prob 811: Base

---
+++ src/SubdomainVisitCount.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubdomainVisitCount {
+    public static void main(String[] args) {
+        String[] domains = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+    }
+
+    public List<String> subdomainVisits(String[] cpdomains) {
+        List<String> ls = new ArrayList<>();
+        return ls;
+    }
+}
