bc4044df9b8e425e1bb41c90102425d067c2f818
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-31 23:13:23 -0600

    Prob 2966

--- src/DivideArrayIntoArraysWithMaxDifference.java
+++ src/DivideArrayIntoArraysWithMaxDifference.java
@@ -8,13 +8,41 @@ public class DivideArrayIntoArraysWithMaxDifference {
         int[] nums = {1,3,4,8,7,9,3,5,1};
         int k = 2;
 
-        System.out.println(divideArray(nums,k));
+        System.out.println(Arrays.deepToString(divideArray(nums,k)));
     }
+
+    // ***************** 1st Method ******************
+    // Approach 1: Check if given array.len % 3 != 0 -> return new int[0][];
+    // Return  int[][] ans = new int[nums.length/3][3], then sort given array
+    // Add the first 3 element of sorted array to ans
+    // While checking nums[i+2]-nums[i] > k, if so return new int[0][];
+    // Runtime  : 23ms        -> + 93.20%
+    // Memory   : 58.26MB     -> + 25.80%
     public static int[][] divideArray(int[] nums, int k) {
-        int[][] ans = new int[4][3];
+        if(nums.length%3 != 0) {
+            return new int[0][];
+        }
 
-        System.out.println(Arrays.deepToString(ans));
+        int[][] ans = new int[nums.length/3][3];
 
+        Arrays.sort(nums);
+        int col = 0; // 0 - 3
+        int i=0;
+        while(i<=nums.length-3) {
+            // initial state
+            if(nums[i+2] - nums[i] > k) {
+                return new int[0][];
+            }
+
+            ans[col][0] = nums[i];
+            ans[col][1] = nums[i+1];
+            ans[col][2] = nums[i+2];
+
+            col++;
+            i += 3;
+        }
         return ans;
     }
+    //  ***************** End of 1st Method ******************
+
 }
