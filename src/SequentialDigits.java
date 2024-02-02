bef28c8598128089a92f60f3c9c2cc10cbc8c26a
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-02 02:54:51 -0600

    Prob 1291

--- src/SequentialDigits.java
+++ src/SequentialDigits.java
@@ -1,25 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SequentialDigits {
     public static void main(String[] args) {
         int low = 100, high = 300;
-        System.out.println(sequentialDigits(100,300));
+        System.out.println(sequentialDigits(1000,13000));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Use nest loop with outer form 1-8 cos 9 has no next sequence
+    // Save i->x, get inner loop from i+1-9, update x by x*10+j
+    // Check if x is between low & high, add to list, sort list & return list
+    // Runtime  : 0ms        -> + 100.00%
+    // Memory   : 40.48MB    -> + 79.58%
     public static List<Integer> sequentialDigits(int low, int high) {
         List<Integer> ls =  new ArrayList<>();
-        int digit = 0, firstDigit = 0;
-        int cp = low;
 
-        while (cp > 0) {
-            if(cp < 10) {
-                firstDigit = cp;
+        for(int i=1; i<9; i++) {
+            int x = i;  // 1-8 cos 9 has no next sequential num
+            for(int j=i+1; j<=9; j++) {
+                x = x * 10 + j; // increment 1 digit each iteration + next sequential num
+                if(x >= low && x <= high) {
+                    ls.add(x);
+                }
             }
-            digit++;
-            cp /= 10;
         }
-        System.out.println(digit + " => " + firstDigit);
+        Collections.sort(ls);
         return ls;
     }
+    //  ***************** End of 1st Method ******************
 }
