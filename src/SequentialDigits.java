f48ece5286021cf965873f6fa456b092d4d9fd9b
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-01 21:52:44 -0600

    Prob 1291: base

---
+++ src/SequentialDigits.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequentialDigits {
+    public static void main(String[] args) {
+        int low = 100, high = 300;
+    }
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ls =  new ArrayList<>();
+
+        return ls;
+    }
+}
