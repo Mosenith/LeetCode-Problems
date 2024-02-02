cfdadee5ffc385324613e29de0063c446baf1bb3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-01 22:11:19 -0600

    Prob 1291: base

--- src/SequentialDigits.java
+++ src/SequentialDigits.java
@@ -4,11 +4,22 @@ import java.util.List;
 public class SequentialDigits {
     public static void main(String[] args) {
         int low = 100, high = 300;
+        System.out.println(sequentialDigits(100,300));
     }
 
-    public List<Integer> sequentialDigits(int low, int high) {
+    public static List<Integer> sequentialDigits(int low, int high) {
         List<Integer> ls =  new ArrayList<>();
+        int digit = 0, firstDigit = 0;
+        int cp = low;
 
+        while (cp > 0) {
+            if(cp < 10) {
+                firstDigit = cp;
+            }
+            digit++;
+            cp /= 10;
+        }
+        System.out.println(digit + " => " + firstDigit);
         return ls;
     }
 }
