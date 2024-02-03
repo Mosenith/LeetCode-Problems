9a858d9c5f694fb2e72a25803754de634f08f6c4
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-02 22:31:22 -0600

    Prob 1043: Base

---
+++ src/PartitionArrayForMaximumSum.java
@@ -0,0 +1,10 @@
+public class PartitionArrayForMaximumSum {
+    public static void main(String[] args) {
+        int[]  arr = {1,15,7,9,2,5,10};
+        int k = 3;
+    }
+
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        return 0;
+    }
+}
