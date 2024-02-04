390d11e0a85f832740221d06341be0a4d22d92de
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-04 02:25:32 -0600

    Prob 811: 1st Approach

--- src/SubdomainVisitCount.java
+++ src/SubdomainVisitCount.java
@@ -14,21 +14,42 @@ public class SubdomainVisitCount {
         for(String s : cpdomains) {
             System.out.println("Curr => " + s);
             String[] sb = s.split("\\s|\\.");
-            System.out.println(Arrays.toString(sb));
 
-            StringBuilder tmp = new StringBuilder();
-            for(int i=1; i<sb.length; i++) {
-                if(i > 1) {
-                    map.computeIfPresent(sb[i], (k,v) -> v + Integer.valueOf(sb[0]));
-                    map.computeIfAbsent(sb[i], v -> Integer.valueOf(sb[0]));
-                }
-                tmp.append(sb[i] + ".");
+            System.out.println(Arrays.toString(sb));
+            if(sb.length == 4) {
+                map.computeIfPresent(sb[3], (k,v) -> v + Integer.valueOf(sb[0]));
+                map.computeIfAbsent(sb[3], v -> Integer.valueOf(sb[0]));
+
+                map.computeIfPresent(sb[2] + "." + sb[3], (k,v) -> v + Integer.valueOf(sb[0]));
+                map.computeIfAbsent(sb[2] + "." + sb[3], v -> Integer.valueOf(sb[0]));
+
+                map.computeIfPresent(sb[1] + "." + sb[2] + "." + sb[3], (k,v) -> v + Integer.valueOf(sb[0]));
+                map.computeIfAbsent(sb[1] + "." + sb[2] + "." + sb[3], v -> Integer.valueOf(sb[0]));
+            } else {
+                // last element: i.e com
+                map.computeIfPresent(sb[2], (k,v) -> v + Integer.valueOf(sb[0]));
+                map.computeIfAbsent(sb[2], v -> Integer.valueOf(sb[0]));
+
+                // all combine
+                map.computeIfPresent(sb[1] + "." + sb[2], (k,v) -> v + Integer.valueOf(sb[0]));
+                map.computeIfAbsent(sb[1] + "." + sb[2], v -> Integer.valueOf(sb[0]));
             }
 
-            System.out.println(tmp);
             System.out.println(map);
             System.out.println("*********\n");
         }
+
+
+        // Iterate over the entries of the map
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            // Concatenate key and value into a string
+            String combined = entry.getValue() + " " + entry.getKey();
+
+            // Add the combined string to the list
+            ls.add(combined);
+        }
+
+
         return ls;
     }
 }
