a0e0791e28d08c0f2f7bcea2c7269da1a50f50c3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-05 22:19:02 -0600

    Base

--- src/BreakAPalindrome.java
+++ src/BreakAPalindrome.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BreakAPalindrome {
     public static void main(String[] args) {
-        String palindrome = "a";
+        String palindrome = "aba";
 
         System.out.println(breakPalindrome(palindrome));
     }
@@ -11,41 +11,29 @@ public class BreakAPalindrome {
         if(palindrome.length() == 1) return "";
 
         char[] org = palindrome.toCharArray();
-        System.out.println("Before --> " + Arrays.toString(org));
 
         for(int i=0; i<org.length; i++) {
-            if(org[i] != 'a') {
-                System.out.println("Change at i = " + i);
+            if(org[i] != 'a' || i == org.length-1) {
                 char tmp = org[i];
-                org[i] = 'a';
+                org[i] = (tmp == 'a') ? 'b' : 'a';
                 String curStr = new String(org);
-                System.out.println(curStr);
                 if(!isPalindrome(curStr)) {
-                    System.out.println("After --> " + Arrays.toString(org));
                     return curStr;
                 } else {
                     org[i] = tmp;
                 }
             }
-            System.out.println("~~~~~\n");
         }
-
-        System.out.println("After --> " + Arrays.toString(org));
-
-        return ";";
+        return "";
     }
 
     public static boolean isPalindrome (String str) {
         char[] c = str.toCharArray();
-        System.out.println("check => " + Arrays.toString(c));
         for(int i=0,j=str.length()-1; i<j; i++,j--) {
             if(c[i] != c[j]) {
-                System.out.println("Is Not A Palindrome");
                 return false;
             }
         }
-
-        System.out.println("Is A Palindrome");
         return true;
     }
 }
