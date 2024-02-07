f504efd4d0d9fc6d30a69dd5d9b6a30095da622d
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-06 23:48:10 -0600

    Prob 451

--- src/SortCharactersByFrequency.java
+++ src/SortCharactersByFrequency.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class SortCharactersByFrequency {
-    public String frequencySort(String s) {
+    public static void main(String[] args) {
+        String s = "tree";
+
+        System.out.println(frequencySort(s));
+    }
+    public static String frequencySort(String s) {
+        int[] alpha = new int[126];
+        Map<Integer,List<Character>> map = new HashMap<>();
+        List<Integer> countList = new ArrayList<>();
+
+        for(char c : s.toCharArray()) {
+            System.out.println(c);
+            ++alpha[c - 'A'];
+        }
+
+//        System.out.println(map);
+        System.out.println(Arrays.toString(alpha));
+
         return "";
     }
 }
