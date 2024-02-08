100663ac9965bfc331268f77eaebf322b2c8a66e
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-08 00:42:22 -0600

    practice algo

--- src/fppQuiz/CommonInterviewCode/DivideCharAndNum.java
+++ src/fppQuiz/CommonInterviewCode/DivideCharAndNum.java
@@ -1,7 +1,30 @@
 package fppQuiz.CommonInterviewCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DivideCharAndNum {
     public static void main(String[] args) {
-        String s = "js12*/[qi";
+        String s = "js12 */[qi";
+
+        List<Character> alphabeticChars = new ArrayList<>();
+        List<Integer> numericChars = new ArrayList<>();
+        List<Character> specialChars = new ArrayList<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == ' ') continue;
+            if(Character.isAlphabetic(c)) {
+                alphabeticChars.add(c);
+            } else if(Character.isDigit(c)) {
+                numericChars.add(Integer.parseInt(String.valueOf(c)));
+            } else {
+                specialChars.add(c);
+            }
+        }
+
+
+        System.out.println(alphabeticChars);
+        System.out.println(numericChars);
+        System.out.println(specialChars);
     }
 }
