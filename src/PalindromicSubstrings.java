be438d41a57c0a5b8f6e18cf90425cb6223acf98
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-12 02:23:52 -0600

    prob 647: base

--- src/PalindromicSubstrings.java
+++ src/PalindromicSubstrings.java
@@ -1,2 +1,12 @@
 public class PalindromicSubstrings {
+    public static void main(String[] args) {
+        // 6 - "a", "a", "a", "aa", "aa", "aaa"
+        String s = "aaa";
+    }
+
+    public int countSubstrings(String s) {
+        int count = s.length();
+
+        return count;
+    }
 }
