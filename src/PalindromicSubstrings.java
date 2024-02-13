e7661620afefad605042119bae1b666d55b7c089
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-13 01:46:11 -0600

    prob 647: 1st Method

--- src/PalindromicSubstrings.java
+++ src/PalindromicSubstrings.java
@@ -2,20 +2,40 @@ public class PalindromicSubstrings {
     public static void main(String[] args) {
         // 6 - "a", "a", "a", "aa", "aa", "aaa"
         String s = "aaa";
-    }
 
-    public int countSubstrings(String s) {
+        System.out.println(countSubstrings(s));
+    }
+    // ***************** 1st Method ******************
+    // Approach 1: DP before optimization & no memoization
+    // Keep track of the len from curIndex to min(curIndex+k, arr.len) while also update maxElement
+    // Find curMax by len * maxElement and + with recursive of same function with index+1
+    // Update maxSum -> max(maxSum, curSum) & return maxSum
+    // Exceed time limit
+    public static int countSubstrings(String s) {
         int count = s.length();
+
+        for(int i=0; i<s.length(); i++) {
+            for(int j=i+1; j<s.length(); j++) {
+                if(isPalindrome(s.substring(i,j+1))) {
+                    System.out.println(i + " : " + j);
+                    System.out.println(s.substring(i,j));
+                    count++;
+                }
+            }
+        }
 
         return count;
     }
-
-    private boolean isPalindrome(String s) {
+    private static boolean isPalindrome(String s) {
         for(int i=0,j=s.length()-1; i<j; i++, j--) {
             if(s.charAt(i) != s.charAt(j))
                 return false;
         }
-
         return true;
+    }
+    //  ***************** End of 1st Method ******************
+
+
+    public static int countSubstrings2(String s) {
     }
 }
