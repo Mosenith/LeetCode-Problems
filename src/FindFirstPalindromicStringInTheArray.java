b399eab849c992ead9ea6cbabd32bc99153b76b5
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-14 15:11:49 -0600

    pr ob 2108 base

---
+++ src/FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,10 @@
+public class FindFirstPalindromicStringInTheArray {
+    public static void main(String[] args) {
+        // ada
+        String[] words = {"abc","car","ada","racecar","cool"};
+    }
+
+    public String firstPalindrome(String[] words) {
+        return "";
+    }
+}
