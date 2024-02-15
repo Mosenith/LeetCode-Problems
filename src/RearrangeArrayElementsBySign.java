52ff3a6e83f5c5e93394b0158e9d2f83062440ac
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-15 00:21:22 -0600

    Prob 2149: base

---
+++ src/RearrangeArrayElementsBySign.java
@@ -0,0 +1,10 @@
+public class RearrangeArrayElementsBySign {
+    public static void main(String[] args) {
+        int[] nums = {3,1,-2,-5,2,-4};
+    }
+
+    public int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        return ans;
+    }
+}
