db85d27cac07ecf577db53f66603d36210fa7f39
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-14 23:47:13 -0600

    Prob 2108: 2nd Approach

--- src/FindFirstPalindromicStringInTheArray.java
+++ src/FindFirstPalindromicStringInTheArray.java
@@ -30,4 +30,23 @@ public class FindFirstPalindromicStringInTheArray {
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Optimized the 1st method into 1 method
+    // Runtime  : 2ms        -> + 58.12%
+    // Memory   : 44.50MB    -> + 95.33%
+    public static String firstPalindrome2(String[] words) {
+        for (var w : words) {
+            boolean ok = true;
+            for (int i = 0, j = w.length() - 1; i < j && ok; ++i, --j) {
+                if (w.charAt(i) != w.charAt(j)) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                return w;
+            }
+        }
+        return "";
+    }
+    //  ***************** End of 2nd Method ******************
 }
