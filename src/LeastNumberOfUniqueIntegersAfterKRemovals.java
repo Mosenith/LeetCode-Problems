7a80bfc1ed8bc6a1f070d1325d2a5b743c0708a0
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-16 01:34:32 -0600

    Prob 1481: Base

---
+++ src/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -0,0 +1,11 @@
+public class LeastNumberOfUniqueIntegersAfterKRemovals {
+    public static void main(String[] args) {
+        // Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+        int[] arr = {4, 3, 1, 1, 3, 3, 2};
+        int k = 3;
+    }
+
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        return 0;
+    }
+}
