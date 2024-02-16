9914e5c9bdd6f12f4e7705266c219aedf904a2ed
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-16 01:09:41 -0600

    Prob 2149: 2nd Approach

--- src/RearrangeArrayElementsBySign.java
+++ src/RearrangeArrayElementsBySign.java
@@ -38,43 +38,29 @@ public class RearrangeArrayElementsBySign {
         return ans;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Use 2 indices, i for >0, j for <0
+    // Loop through nums[], if >0, have ans[i] = n, then i+=2
+    // Similarly for <0
+    // Runtime  : 3ms        -> + 100.00%
+    // Memory   : 77.20MB    -> + 42.75%
     public static int[] rearrangeArray2(int[] nums) {
         int[] ans = new int[nums.length];
-        int index = 0;
-        Queue<Integer> positive = new ArrayDeque<>();
-        Queue<Integer> negative = new ArrayDeque<>();
+        int i=0, j=1;   // i for positive, j for negative
 
         for(int n : nums) {
-            if(index%2==0 && n<0) {
-                negative.add(n);
-                continue;
-            } else if(index%2!=0 && n>0) {
-                positive.add(n);
-                continue;
-            }
-
-            if(n>0 && positive.isEmpty()) {
-                ans[index] = n;
-            } else if(n>0 && !positive.isEmpty()){
-                positive.add(n);
-                ans[index] = positive.poll();
-            } else if(n<0 && negative.isEmpty()) {
-                ans[index] = n;
+            if(n > 0) {
+                ans[i] = n;
+                i += 2;
             } else {
-                negative.add(n);
-                ans[index] = negative.poll();
-            }
-            index++;
-        }
-
-        while(index < ans.length) {
-            if(index % 2 == 0) {
-                ans[index++] = positive.poll();
-            } else {
-                ans[index++] = negative.poll();
+                ans[j] = n;
+                j += 2;
             }
         }
 
         return ans;
     }
+    //  ***************** End of 2nd Method ******************
+
 }
