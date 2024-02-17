f00db044c516640ea14e3807472021007457b2a3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-17 02:26:14 -0600

    Prob 1481: 2nd Approach

--- src/LeastNumberOfUniqueIntegersAfterKRemovals.java
+++ src/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LeastNumberOfUniqueIntegersAfterKRemovals {
     public static void main(String[] args) {
@@ -9,7 +6,7 @@ public class LeastNumberOfUniqueIntegersAfterKRemovals {
         int[] arr = {4, 3, 1, 1, 3, 3, 2};
         int k = 3;
 
-        System.out.println(findLeastNumOfUniqueInts(arr,k));
+        System.out.println(findLeastNumOfUniqueInts2(arr,k));
     }
 
     // ***************** 1st Method ******************
@@ -35,15 +32,44 @@ public class LeastNumberOfUniqueIntegersAfterKRemovals {
                 k -= entry.getValue();
                 map.remove(entry.getKey());
             }
-            if(k == 0)
+            if(k <= 0)
                 break;
         }
         return map.size();
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Optimized 1st method by using List<Integer> of map.values instead of List<Entry>
+    // Sort this list and iterate from i=0 till i<list.size & if k<0 return len-i
+    // Each iteration k-=list(i) and if out of loop return 0
+    // Runtime  : 37ms       -> + 92.29%
+    // Memory   : 56.70MB    -> + 80.19%
     public static int findLeastNumOfUniqueInts2(int[] arr, int k) {
+        Map<Integer, Integer> cnt = new HashMap<>();
 
+        // Count the frequency of each element
+        for (int x : arr) {
+            cnt.merge(x, 1, Integer::sum);
+        }
+
+        // Get value() in map to List & sort it
+        List<Integer> nums = new ArrayList<>(cnt.values());
+        Collections.sort(nums);
+
+        // Remove value from the smallest till k<0;
+        for(int i=0, len=nums.size(); i<len; i++) {
+            k -= nums.get(i);
+
+            //**Important** Need to loop till k<0 cos i starts from 0
+            // And we want to stop removing elements once we have removed exactly k elements
+            if(k < 0) {
+                return len-i;
+            }
+        }
+
+        // If out of loop means we need to remove everything in the arr[]
+        return 0;
     }
-
+    //  ***************** End of 2nd Method ******************
 }
