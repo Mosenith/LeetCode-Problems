24b3ed3971035b6992f4ac357c77f47ddc0ee452
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-19 17:55:43 -0600

    Prob 788: 1st approach

--- src/RotatedDigits.java
+++ src/RotatedDigits.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class RotatedDigits {
     public static void main(String[] args) {
         int n = 10; // 4 : 2, 5, 6, 9
@@ -7,18 +5,24 @@ public class RotatedDigits {
         System.out.println(rotatedDigits(857));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Check from 1 to n if it's valid or not
+    // Check valid by init flag=false, if a digit=2,5,6,9 -> set flag=true
+    // If encounter 3,4,7 => return false, out of loop return flag
+    // Runtime  : 3ms           -> + 94.20%
+    // Memory   : 40.11MB       -> + 85.32%
     public static int rotatedDigits(int n) {
         int count = 0;
 
         for(int i=1; i<=n; i++) {
-            if(checkDigits(i)) {
+            if(isValid(i)) {
                 count++;
             }
         }
         return count;
     }
 
-    private static boolean checkDigits(int i, Set<Integer> set) {
+    private static boolean isValid(int i) {
         boolean flag = false;
         while(i > 0) {
             int tmp = i % 10;
@@ -32,4 +36,5 @@ public class RotatedDigits {
         }
         return flag;
     }
+    //  ***************** End of 1st Method ******************
 }
