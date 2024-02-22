384f2ca1b2d0b104703fd6a247bcbf4dff508c7f
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-21 21:18:25 -0600

    Prob 201: 1st & 2nd Approach

--- src/BitwiseAndOfNumbersRange.java
+++ src/BitwiseAndOfNumbersRange.java
@@ -1,20 +1,45 @@
 public class BitwiseAndOfNumbersRange {
     public static void main(String[] args) {
-        System.out.println(rangeBitwiseAnd(1073741824,2147483647));
+        System.out.println(rangeBitwiseAnd2(5,7));
     }
 
+    // 4 - 0100
+    // 5 - 1001
+    // 6 - 1100
+    // 7 - 1101
+    // 8 - 1000
     // ***************** 1st Method ******************
-    // Approach 1: Work but Time Limit Exceeded
+    // Approach 1: let ans = left & right, continue ans &= (left+1) & (right-1)
+    // After loop, check if i=j -> ans &= i, then return ans
+    // Passed all tests but Time Limit Exceeded
     public static int rangeBitwiseAnd(int left, int right) {
-        int ans = left;
-        for(int i=left+1; i<=right; i++) {
-            ans = ans & i;
+        int ans = left & right;
+        int i=left+1,j=right-1;
+
+        // i>0 to avoid overflow
+        for(; i<j && i>0; i++,j--) {
             if(ans == 0) {
                 return 0;
             }
+            ans = ans & (i & j);
+        }
+        if(i == j) {
+            ans &= i;
         }
         return ans;
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: start from right and & with (right-1) until right <= left
+    // Runtime  : 3ms        -> + 100.00%
+    // Memory   : 77.20MB    -> + 42.75%
+    public static int rangeBitwiseAnd2(int left, int right) {
+        while (left < right) {
+            System.out.println(left + " : " + right);
+            right &= (right - 1);
+        }
+        return right;
+    }
+    //  ***************** End of 2nd Method ******************
 }
