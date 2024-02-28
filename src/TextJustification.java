d86be05164c4259ff2ad13880da6374493eae0e0
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-27 21:03:48 -0600

    Prob 68 base

---
+++ src/TextJustification.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextJustification {
+    public static void main(String[] args) {
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        int maxWidth = 16;
+    }
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
