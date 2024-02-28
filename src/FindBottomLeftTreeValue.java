6f03f2654f0eaedd17f7ba158345ad30d7f2df3f
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-02-28 01:11:55 -0600

    Prob 513: base

---
+++ src/FindBottomLeftTreeValue.java
@@ -0,0 +1,35 @@
+public class FindBottomLeftTreeValue {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void printTreeNode(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        printTreeNode(root.left);
+        printTreeNode(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+        root1.left.left = new TreeNode(4);
+
+        System.out.println(findBottomLeftValue(root1));
+    }
+
+    public static int findBottomLeftValue(TreeNode root) {
+        return 0;
+    }
+}
