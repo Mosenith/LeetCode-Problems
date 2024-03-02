0f3f258968a234f95e49ed57e959b6979a4cb7f2
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-02 04:59:31 -0600

    Prob 833: 1st Approach

--- src/FindAndReplaceInString.java
+++ src/FindAndReplaceInString.java
@@ -4,57 +4,53 @@ import java.util.Map;
 
 public class FindAndReplaceInString {
     public static void main(String[] args) {
-        // abcde
-        String s = "abcde"; // eeebffff
-        int[] indices = {2,2};
-        String[] sources = {"cde","cdef"};
-        String[] targets = {"fe","f"};
+        String s = "eqfvuokjvnlkyrgdnzem"; // eeebffff
+        int[] indices = {13,0,8,2,4,16};
+        String[] sources = {"rgd","eq","vnlky","fv","uo","nze"};
+        String[] targets = {"bmx","skn","ckcw","xso","c","sbmo"};
 
         System.out.println(findReplaceString(s,indices,sources,targets));
     }
+    // ***************** 1st Method ******************
+    // Approach 1: Use array d[] to store the location of indices where sources exist in s
+    // So if d[i]>=0, we need to append target to ans
+    // Otherwise, just add char in s to ans
+    // Runtime  : 1ms           -> + 100.00%
+    // Memory   : 42.26MB       -> + 71.83%
     public static String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        StringBuilder str = new StringBuilder();
-        // index for moving along s to str, i for other arrays
-        int index = 0, i = 0;
-        Map<Integer,Integer> map = new HashMap<>();
+        int n = s.length();
+        var d = new int[n];
+        Arrays.fill(d, -1);
 
-        for(int j=0; j<indices.length; j++) {
-            map.put(indices[j], j);
-        }
-        Arrays.sort(indices);
-
-        while(index < s.length() & i <indices.length) {
-            // i uses with indices, others uses prePos
-            int prePos = map.get(indices[i]);
-
-            // add remaining char in s to str
-            while(index < s.length() && indices[i] != index) {
-                str.append(s.charAt(index++));
+        // compare source to s whether the start of char in source exist at i-th index or not
+        // If so, set d[i] to curIndex within indices[]
+        for (int k = 0; k < indices.length; ++k) {
+            int i = indices[k];
+            System.out.println(sources[k] + " ; " + i);
+            if (s.startsWith(sources[k], i)) {
+                System.out.println("Yes at i=" + i);
+                d[i] = k;
             }
-
-            int end = index + sources[prePos].length();
-            if(end > s.length()) {
-                break;
-            }
-
-            // if not a substring of s, add org to str and continue
-            if(!s.substring(index,end).equals(sources[prePos])) {
-                str.append(s.substring(index,end));
-                index = end;
-                i++;
-                continue;
-            }
-
-            // if is a substring of s, add the targets to str
-            str.append(targets[prePos]);
-            i++;
-            index = end;
         }
 
-        while(index < s.length()) {
-            str.append(s.charAt(index++));
+        System.out.println(Arrays.toString(d));
+
+        var ans = new StringBuilder();
+
+        // loop from 0 to s.len
+        // if d[i] >= 0, ans.append targets[d[i]], then i +=  sources[d[i]].length()
+        // Otherwise, ans.append s.at(i++)
+        for (int i = 0; i < n;) {
+            if (d[i] >= 0) {
+                ans.append(targets[d[i]]);
+                i += sources[d[i]].length();
+            } else {
+                ans.append(s.charAt(i++));
+            }
         }
 
-        return str.toString();
+        return ans.toString();
     }
+    //  ***************** End of 1st Method ******************
+
 }
