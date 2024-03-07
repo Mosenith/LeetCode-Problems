05904348709366a3f4ceadc1b796e749591f73c4
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-07 02:13:41 -0600

    prob 467: 1st Approach

--- src/UniqueSubstringsInWraparoundString.java
+++ src/UniqueSubstringsInWraparoundString.java
@@ -1,18 +1,41 @@
+import java.util.Arrays;
+
 public class UniqueSubstringsInWraparoundString {
     public static void main(String[] args) {
-        String s = "zab";
+        String s = "abaab";
 
         System.out.println(findSubstringInWraproundString(s));
     }
 
-    // diff should always be 1 or 'z' follows by 'a'
-    // If all char diff btw each other is 1 or 'za' => signma from 0 to n = n*(n+1)/2
-    // If not => add # chars
+    // ***************** 1st Method ******************
+    // Approach 1: Use an array size of 26 keep track of the maximum length of
+    // substring ending at each character. Init k=,
+    // which represents the length of the current increasing substring
+    // If i > 0 and the difference between the ASCII values of c
+    // and the previous character (s.charAt(i - 1)) is 1 (modulo 26),
+    // it means c is the next character in the wraparound string, so k is incremented.
+    // Otherwise, k is reset to 1.
+    // The length of the substring ending at character c is updated in the dp array.
+    // Runtime  : 8ms           -> + 89.07%
+    // Memory   : 43.07MB       -> + 93.99%
     public static int findSubstringInWraproundString(String s) {
-
-        for(char c : s.toCharArray()) {
-            System.out.println(c + " => " + Character.getNumericValue(c));
+        int[] dp = new int[26];
+        int k = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (i > 0 && (c - s.charAt(i - 1) + 26) % 26 == 1) {
+                ++k;
+            } else {
+                k = 1;
+            }
+            dp[c - 'a'] = Math.max(dp[c - 'a'], k);
         }
-        return 0;
+
+        int ans = 0;
+        for (int v : dp) {
+            ans += v;
+        }
+        return ans;
     }
+    //  ***************** End of 1st Method ******************
 }
