204d0eaf65535bab9e12742b8cac293ee5053d88
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-07 03:13:00 -0600

    Prob 229: base

---
+++ src/MajorityElementII.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElementII {
+    public static void main(String[] args) {
+        int[] nums = {3,2,3}; // 3
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
