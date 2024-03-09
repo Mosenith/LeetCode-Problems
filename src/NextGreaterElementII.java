f8432890821669a96de8200d0b05dbb48c3d5bd3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-09 04:59:01 -0600

    prob 503: 1st approach

---
+++ src/NextGreaterElementII.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class NextGreaterElementII {
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1};
+
+        System.out.println(Arrays.toString(nextGreaterElements(nums)));
+    }
+
+    // ***************** 1st Method ******************
+    // Approach 1:Loop from i+1 to nums.len, if encounter greater one, add to ans[i]
+    // Otherwise, means next greater is not on the right, look from the left to i-1
+    // If left=i, means it's the max element so ans[i]=-1
+    // Runtime  : 7ms           -> + 68.12%
+    // Memory   : 43.26MB       -> + 57.96%
+    public static int[] nextGreaterElements(int[] nums) {
+        int[] ans = new int[nums.length];
+        int len = nums.length;
+
+        for(int i=0; i<len; i++) {
+            int right = i+1;
+            while(right<len) {
+                if(nums[right] > nums[i]) {
+                    ans[i] = nums[right];
+                    break;
+                }
+                right++;
+            }
+            if(right == len) {
+                int left = 0;
+                while(left<i) {
+                    if(nums[left] > nums[i]) {
+                        ans[i] = nums[left];
+                        break;
+                    }
+                }
+                if(left == i) {
+                    ans[i] = -1;
+                }
+            }
+        }
+        return ans;
+    }
+    //  ***************** End of 1st Method ******************
+
+}
