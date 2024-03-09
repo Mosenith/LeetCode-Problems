1f4382909d387442fc139fb268ce92e7c0926e7c
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-09 02:35:39 -0600

    prob 3005: 2nd approach

--- src/CountElementsWithMaximumFrequency.java
+++ src/CountElementsWithMaximumFrequency.java
@@ -1,13 +1,11 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CountElementsWithMaximumFrequency {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1,2,2,3,1,4};
 
-        System.out.println(maxFrequencyElements(nums));
+        System.out.println(maxFrequencyElements2(nums));
     }
 
     // ***************** 1st Method ******************
@@ -43,4 +41,35 @@ public class CountElementsWithMaximumFrequency {
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Sort given array first and loop through the array
+    // Count the duplicate element while keeping tract of the maxFreq, also add all freq to the list
+    // Loop through the list while comparing to the maxFreq, if equal, plus to the ans
+    // Runtime  : 2ms         -> + 70.78%
+    // Memory   : 42.07MB     -> + 79.03%
+    public static int maxFrequencyElements2(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        int maxFreq = 0;
+        List<Integer> ls = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int cur = nums[i];
+            int freq = 1;
+            while(i+1<nums.length && nums[i+1] == cur) {
+              freq++;
+              i++;
+            }
+            maxFreq = Math.max(maxFreq, freq);
+            ls.add(freq);
+        }
+
+        for(int n : ls) {
+            if(maxFreq == n) {
+                count += n;
+            }
+        }
+        return count;
+    }
+    //  ***************** End of 2nd Method ******************
 }
