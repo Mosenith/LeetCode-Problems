9cc2d479593270aaff917ca4f6d69f6c03cc9131
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-12 02:11:35 -0500

    prob 791: base

--- src/CustomSortString.java
+++ src/CustomSortString.java
@@ -1,9 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class CustomSortString {
     public static void main(String[] args) {
-
+        String order = "cba", s = "abcd";
     }
 
+    // s = "abcabcd"
     public String customSortString(String order, String s) {
+        char[] orderArray = order.toCharArray();
+        Queue<Integer> queue = new ArrayDeque<>();
+        // read everything in s to map with k-s.charAt(i), v-s.charAt(i) occurrence while checking if it exist in orderSet
+        // If not get it to another list or sth
 
+
+
+
+
+        // Loop in orderArray, if charA in map set it to ans, then reduce it occurrence(>1) or remove it(==1)
+        // Out of loop check map, if not empty, get all those keys to ans, doesn't matter about the order
     }
 }
