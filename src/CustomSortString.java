3d1b6f9b4f095dabb76cdeb0112e42d2485617d3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-13 14:21:15 -0500

    Prob 791: 1st Approach

--- src/CustomSortString.java
+++ src/CustomSortString.java
@@ -2,38 +2,60 @@ import java.util.*;
 
 public class CustomSortString {
     public static void main(String[] args) {
-        String order = "cba", s = "abcd";
+        // s = "abcabcd"
+        String order = "kqep", s = "pekeq";
 
         System.out.println(customSortString(order,s));
     }
 
-    // s = "abcabcd"
+    // ***************** 1st Method ******************
+    // Approach 1: Read order to Set<Char> to use contains(obj) with s
+    // Read chars in s to map with k-s.charAt(i), v-s.charAt(i) occurrence
+    // while checking if it exists in orderSet. If not get it to another map
+    // Loop through charArray of order, if existMap has order[i], append to ans till occurrence=0 then remove it
+    // Add the leftover of nonExistMap to ans
+    // Runtime  : 4ms          -> + 28.59%
+    // Memory   : 41.34MB      -> + 65.54%
     public static String customSortString(String order, String s) {
+        char[] orderArray = order.toCharArray();
         Set<Character> orderSet = new HashSet<>();
 
-        for(char c : order.toCharArray()) {
+        for(char c : orderArray) {
             orderSet.add(c);
         }
-        System.out.println(orderSet);
-        // read everything in s to map with k-s.charAt(i), v-s.charAt(i) occurrence while checking if it exist in orderSet
-        // If not get it to another list or sth
 
-        Map<Character,Integer> existChar = new HashMap<>();
-        Map<Character,Integer> nonExistChar = new HashMap<>();
 
+        Map<Character,Integer> existMap = new HashMap<>();
+        Map<Character,Integer> nonExistMap = new HashMap<>();
         for(char c : s.toCharArray()) {
             if(orderSet.contains(c)) {
-                existChar.put(c,existChar.getOrDefault(c,0)+1);
+                existMap.put(c,existMap.getOrDefault(c,0)+1);
             } else {
-                nonExistChar.put(c,nonExistChar.getOrDefault(c,0)+1);
+                nonExistMap.put(c,nonExistMap.getOrDefault(c,0)+1);
             }
         }
 
-        System.out.println(existChar);
-        System.out.println(nonExistChar);
+        StringBuilder ans = new StringBuilder();
+        for(int i=0; i<orderArray.length && !existMap.isEmpty(); i++) {
+            char cur = orderArray[i];
+            if(existMap.containsKey(cur)) {
+                while(existMap.get(cur) >= 1) {
+                    ans.append(cur);
+                    existMap.put(cur,existMap.getOrDefault(cur,0)-1);
+                }
+                existMap.remove(cur);
+            }
+        }
 
-        // Loop in orderArray, if charA in map set it to ans, then reduce it occurrence(>1) or remove it(==1)
-        // Out of loop check map, if not empty, get all those keys to ans, doesn't matter about the order
-        return "";
+        for(Character c : nonExistMap.keySet()) {
+            while(nonExistMap.get(c) >= 1) {
+                ans.append(c);
+                nonExistMap.put(c, nonExistMap.get(c)-1);
+            }
+        }
+
+        return ans.toString();
     }
+    //  ***************** End of 1st Method ******************
+
 }
