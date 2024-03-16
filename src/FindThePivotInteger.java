8948074dedd5f60e9aa861141c5906019fc07ccc
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-15 21:58:30 -0500

    Prob 930: 2nd Approach

--- src/FindThePivotInteger.java
+++ src/FindThePivotInteger.java
@@ -14,7 +14,7 @@ public class FindThePivotInteger {
     // Otherwise, +r to rightSum then r--
     // Out of loop, if leftSum==rightSum && rightSum!=n, return l, otherwise -1
     // Runtime  : 1ms          -> + 61.93%
-    // Memory   : 40.11MB      -> + 92.55%
+    // Memory   : 40.50MB      -> + 60.79%
     public static int pivotInteger(int n) {
         if(n == 1) return 1;
         int leftSum = 1, rightSum = n;
@@ -34,5 +34,22 @@ public class FindThePivotInteger {
         return (leftSum==rightSum && rightSum!=n) ? l : -1;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Compare sum from the left & sum from the right
+    // From left, (1+x)(x)/2 and from right, (x+n)(#num of elements)/2
+    // Basically, just compare (1+x)(x) with (x+n)(#num of elements)
+    // If equal just return x, otherwise out of loop return -1
+    // Runtime  : 1ms           -> + 61.93%
+    // Memory   : 40.11MB       -> + 92.55%
+    public static int pivotInteger2(int n) {
+        for (int x = 1; x <= n; ++x) {
+            if ((1 + x) * x == (x + n) * (n - x + 1)) {
+                return x;
+            }
+        }
+        return -1;
+    }
+    //  ***************** End of 2nd Method ******************
 
 }
