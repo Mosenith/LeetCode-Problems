145e0efbce700cea2b498e00717edc1babf5375e
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-16 02:29:41 -0500

    Prob 930: 2nd approach

--- src/BinarySubarraysWithSum.java
+++ src/BinarySubarraysWithSum.java
@@ -1,7 +1,9 @@
 public class BinarySubarraysWithSum {
     public static void main(String[] args) {
         int[] nums = {1,0,1,0,1};
-        int goal = 2;
+        int goal = 0;
+
+        System.out.println(numSubarraysWithSum2(nums,2));
     }
 
     public static int numSubarraysWithSum(int[] nums, int goal) {
@@ -16,5 +18,27 @@ public class BinarySubarraysWithSum {
             ++cnt[s];
         }
         return ans;
+    }
+
+    public static int numSubarraysWithSum2(int[] nums, int goal) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans += countSubarrays(nums, goal, i);
+        }
+        return ans;
+    }
+
+    private static int countSubarrays(int[] nums, int goal, int start) {
+        int count = 0;
+        int curSum = 0;
+        for (int i = start; i < nums.length; i++) {
+            curSum += nums[i];
+            if (curSum == goal) {
+                count++;
+            } else if (curSum > goal) {
+                break; // No need to continue as sum can't be achieved further
+            }
+        }
+        return count;
     }
 }
