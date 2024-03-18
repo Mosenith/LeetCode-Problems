4a1d87816870891e96615f26934e6df3ad5ef2a2
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-17 23:54:18 -0500

    Prob 930: 3rd approach

--- src/BinarySubarraysWithSum.java
+++ src/BinarySubarraysWithSum.java
@@ -1,11 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BinarySubarraysWithSum {
     public static void main(String[] args) {
         int[] nums = {1,0,1,0,1};
-        int goal = 0;
+        int goal = 2;
 
-        System.out.println(numSubarraysWithSum2(nums,2));
+        System.out.println(numSubarraysWithSum3(nums,2));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Use memoize(cnt[]) by keeping track of sum & each iteration ++cnt[s]
+    // If sum-goal>=0, add ans += cnt[sum-goal]
+    // Runtime  : 3ms          -> + 73.47%
+    // Memory   : 48.42MB      -> + 35.73%
     public static int numSubarraysWithSum(int[] nums, int goal) {
         int[] cnt = new int[nums.length + 1];
         cnt[0] = 1;
@@ -19,7 +27,12 @@ public class BinarySubarraysWithSum {
         }
         return ans;
     }
+    //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Use recursion, not the best time complexity but good memory usage
+    // Runtime  : 1790ms        -> + 14.09%
+    // Memory   : 47.01MB       -> + 95.54%
     public static int numSubarraysWithSum2(int[] nums, int goal) {
         int ans = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -41,4 +54,24 @@ public class BinarySubarraysWithSum {
         }
         return count;
     }
+    //  ***************** End of 2nd Method ******************
+
+    // ***************** 3rd Method ******************
+    // Approach 3: Similar to 1st approach but use map
+    // Runtime  : 21ms          -> + 52.78%
+    // Memory   : 49.16MB       -> + 27.99%
+    public static int numSubarraysWithSum3(int[] nums, int goal) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // Initialize with 0 sum occurring once
+
+        for (int num : nums) {
+            sum += num;
+            count += map.getOrDefault(sum - goal, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+    //  ***************** End of 3rd Method ******************
 }
