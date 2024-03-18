c1bf82c4e9e68a6f2f20df54b85bc60ad6bfef2c
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-18 18:53:21 -0500

    Prob 452: base

--- src/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MinimumNumberOfArrowsToBurstBalloons {
     public static void main(String[] args) {
         int[][] points = {
@@ -6,9 +8,18 @@ public class MinimumNumberOfArrowsToBurstBalloons {
                 {1, 6},
                 {7, 12}
         };
+
+        System.out.println(findMinArrowShots(points));
     }
 
-    public int findMinArrowShots(int[][] points) {
+    // Sort 2D array based on the arr[][1]
+    // Cur end=arr[i][0], then check if arr[i][1] is within the arr[i+1][0],
+    // if so end=arr[i+1][1], if not shot++.
+    // [[1,6],[2,8],[7,12],[10,16]]
+    public static int findMinArrowShots(int[][] points) {
+
+
+        System.out.println(Arrays.deepToString(points));
         return 0;
     }
 }
