523c60fb33d6029f16720cf12ebe0981bf143052
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-19 03:51:51 -0500

    Prob 452: 1st Approach

--- src/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/MinimumNumberOfArrowsToBurstBalloons.java
@@ -10,27 +10,39 @@ public class MinimumNumberOfArrowsToBurstBalloons {
                 {7, 12}
         };
 
+        int[][] points2 = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8}
+        };
+
         System.out.println(findMinArrowShots(points));
     }
-// [[1,2],[2,3],[3,4],[4,5]]
-
-    // Sort 2D array based on the arr[][1]
-    // Cur end=arr[i][0], then check if arr[i][1] is within the arr[i+1][0],
-    // if so end=arr[i+1][1], if not shot++.
+    // [[1,2],[2,3],[3,4],[4,5]]
     // [[1,6],[2,8],[7,12],[10,16]]
+
+
+    // ***************** 1st Method ******************
+    // Approach 1: Sort 2D array based on the arr[][1]
+    // Init curEnd=arr[0][1], then check if arr[i][0]>curEnd, update curEnd=arr[i][1]
+    // Then increment shot++
+    // Runtime  : 57ms         -> + 34.97%
+    // Memory   : 68.67MB      -> + 77.08%
     public static int findMinArrowShots(int[][] points) {
-        int shots = 0;
+        if (points.length == 0) return 0;
         // Sorting based on the second column (index 1)
         Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
-        System.out.println(Arrays.deepToString(points));
 
         int curEnd = points[0][1];
+        int shots = 1;
         for(int i=1; i<points.length; i++) {
-            if(points[i][0] < curEnd) {
-
+            if (points[i][0] > curEnd) {
+                shots++;
+                curEnd = points[i][1];
             }
         }
-
         return shots;
     }
+    //  ***************** End of 1st Method ******************
 }
