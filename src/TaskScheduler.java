6f8f3980c0da0dc455ea3ddfc5ed6c1cbd62dc0b
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-20 00:13:14 -0500

    Prob 621: base

--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -1,9 +1,49 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class TaskScheduler {
     public static void main(String[] args) {
+        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+        int n = 2;
 
+        System.out.println(leastInterval(tasks,n));
     }
 
-    public int leastInterval(char[] tasks, int n) {
-        return 0;
+    public static int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> intakeMap = new HashMap<>();
+        Queue<Character> waitQueue = new LinkedList<>();
+        int intervals = 0, coolDown = 0;
+
+        for(int i=0; i<tasks.length; i++) {
+            char c = tasks[i];
+            System.out.println("at i=" + i + " => c=" + c);
+            if(!waitQueue.isEmpty()) {
+                if(waitQueue.peek() != c) {
+                    coolDown++;
+                } else {
+                    waitQueue.add(c);
+                    continue;
+                }
+            }
+            if(intakeMap.isEmpty() || !intakeMap.containsKey(c) || intakeMap.get(c) < n) {
+                intakeMap.put(c,intakeMap.getOrDefault(c,0) + 1);
+                intervals++;
+            } else {
+                if(intakeMap.containsKey(c) && intakeMap.get(c) == n) {
+                    intakeMap.remove(c);
+                }
+                waitQueue.add(c);
+            }
+            if(coolDown == n) {
+                intervals++;
+                coolDown = 0;
+            }
+            System.out.println(intakeMap);
+            System.out.println(waitQueue);
+            System.out.println("***********\n");
+        }
+        return intervals;
     }
 }
