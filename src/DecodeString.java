17cee50c715ee0d7016243c375c1d6a9a8a0a720
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-22 15:09:18 -0500

    Prob 394: base

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -8,60 +8,18 @@ public class DecodeString {
         String s2 = "2[abc]3[cd]ef";    // "abcabccdcdcdef"
         String s3 = "3[a]2[bc]";    // "aaabcbc"
 
-        System.out.println(decodeString(s3));
+        System.out.println(decodeString(s1));
     }
     // num,[,char,] -> 4*n-1
     // 8-1=7
     public static String decodeString(String s) {
         StringBuilder sb = new StringBuilder();
-        Stack<String> stk = new Stack<>();
 
         System.out.println("===== Start Func =====");
-        for(int i=0; i<s.length(); i++) {
-            int prev = i;
-            while(prev<s.length() && !Character.isAlphabetic(s.charAt(prev))){
-                prev++;
-            }
+        for(int i=0; i<s.length(); i++){
+            int num = Character.getNumericValue(s.charAt(i));
 
-            System.out.println("mid prev = " + prev);
-            StringBuilder tmp = new StringBuilder();
-            while(prev<s.length() && Character.isAlphabetic(s.charAt(prev))){
-                tmp.append(s.charAt(prev++));
-            }
-
-            if(!tmp.isEmpty()) {
-                stk.add(tmp.toString());
-            }
-            i = prev;
-            System.out.println("cur i=" + i);
-            System.out.println("********\n");
         }
-        System.out.println(stk);
-
-
-        // start from right till meet numeric
-        for(int i=s.length()-1; i>=0; i--) {
-            System.out.println(i + " -> " + s.charAt(i));
-            if(s.charAt(i) == '[') {
-                int next = i;
-                while(s.charAt(next) != ']') {
-                    next++;
-                }
-                String cur = s.substring(i, next);
-                if(stk.peek() != cur) {
-                    sb.append(stk.pop());
-                } else {
-                    String loopChar = stk.pop();
-                    int time = Character.getNumericValue(s.charAt(i-1));
-                    // i-1 is numeric, so loop i-1 times
-                    for(int j=0; j<time; j++) {
-                        sb.insert(0,loopChar);
-                    }
-                }
-            }
-            System.out.println("************\n");
-        }
-
 
         return sb.toString();
     }
