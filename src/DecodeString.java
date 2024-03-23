25c5338de953a20edd927ff9782b8215213dbf18
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-23 00:16:46 -0500

    Prob 394: base

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -5,8 +5,9 @@ public class DecodeString {
         String s1 = "3[a2[c]]";  // "accaccacc"
         String s2 = "2[abc]3[cd]ef";    // "abcabccdcdcdef"
         String s3 = "3[a]2[bc]";    // "aaabcbc"
+        String s4 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
 
-        System.out.println(decodeString(s3));
+        System.out.println(decodeString(s4));
     }
     // num,[,char,] -> 4*n-1
     // 8-1=7
@@ -21,7 +22,13 @@ public class DecodeString {
                 sb.append(s.charAt(i));
                 continue;
             }
-            int num = Character.getNumericValue(s.charAt(i));
+            int prev = i;
+            // get numeric
+            while(Character.isDigit(s.charAt(i)) && s.charAt(i+1) != '[') {
+                i++;
+            }
+            System.out.println("numeric -> " + s.substring(prev,i+1));
+            int num = Integer.parseInt(s.substring(prev,i+1));
             int next = i+2;
 
             while(s.charAt(next) != ']') {
@@ -30,16 +37,20 @@ public class DecodeString {
                     // recursion
                     System.out.println(next + " recursion => s = " + s.substring(next));
                     ls.add(decodeString(s.substring(next)));
+                    break;
                 }
             }
-
+            StringBuilder tmp = new StringBuilder();
             if(ls.isEmpty()) {
-                StringBuilder tmp = new StringBuilder(s.substring(i+2,next));
-                sb.append(String.valueOf(tmp).repeat(Math.max(0, num)));
+                tmp.append(s, i+2, next);
+                i = next;
             } else {
-
+                System.out.println(ls + " & " + s.substring(i+2,next));
+                tmp.append(s.substring(i+2,next) + ls.get(0));
+                i = next + ls.get(0).length();
             }
-            i = next;
+            sb.append(String.valueOf(tmp).repeat(Math.max(0, num)));
+
             System.out.println("cur => " + sb);
             System.out.println("****************\n");
         }
