2078a7c55d37b850a16ff7e4f85d01bd4e7a65e5
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-25 19:13:14 -0500

    Prob 394: 2nd Appraoch

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -7,13 +7,13 @@ public class DecodeString {
         String s3 = "3[a]2[bc]";    // "aaabcbc"
         String s4 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
 
-        System.out.println(decodeString(s2));
+        System.out.println(decodeString2(s1));
     }
 
     // ***************** 1st Method ******************
     // Approach : Use 2 stack, one for counting the repeated times, when encounters ']' add count to countStack
-    // Another one for storing the encountered chars, when encounters '[' add currentString & reset currentString and count
-    // When encounter ']', pop from stringStack and append to ans with countStack.pop() times
+    // Another one for storing already cal chars, when encounters '[' add currentString & reset currentString and count
+    // When encounter ']', pop from stringStack and append to currentString with countStack.pop() times
     // Otherwise, append ch to currentString
     // Runtime  : 0ms        -> + 100%
     // Memory   : 41.35 MB   -> + 69.14%
@@ -24,7 +24,6 @@ public class DecodeString {
         StringBuilder currentString = new StringBuilder();
 
         for (char ch : s.toCharArray()) {
-            System.out.println("Iteration -> " + ch);
             if (Character.isDigit(ch)) {
                 // get number of repeated times
                 count = count * 10 + (ch - '0');
@@ -42,35 +41,31 @@ public class DecodeString {
                 currentString = decodedString;
             } else {
                 // without repeated time
-                System.out.println("No repeated character: " + ch);
                 currentString.append(ch);
             }
-            System.out.println("count = " + count + ", currentString = " + currentString);
-            System.out.println(countStack);
-            System.out.println(stringStack);
-            System.out.println("*****************\n");
         }
         return currentString.toString();
     }
     //  ***************** End of 1st Method ******************
 
     // ***************** 2nd Method ******************
-    // Approach 2: Use Stack and read all nodes store in stack
-    // Pop out one by one to ansListNode
-    // Runtime  : 1ms         -> + 5.28%
-    // Memory   : 45.61MB     -> + 58.12%
-    // using recursion
+    // Approach 2: Using recursion start from 0 to s.len-1
+    // Check if numeric, get numeric and using loop to iterate until we get to ']'
+    // Then call recursion from prevI to i-1 => we'll get the inner char btw []
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 41.02MB     -> + 94.62%
+    // 3[a2[c]]
     public static String decodeString2(String s) {
         return helper(s,0,s.length()-1);
     }
     private static String helper(String s, int l , int r){
         if(l>r) return "";
         StringBuilder sb=new StringBuilder();
-
         for(int i=l;i<=r;i++){
             char c=s.charAt(i);
             if(c>='0' && c<='9'){
                 int k=0;
+                // this is necessary when numeric >=10
                 while(i<s.length() && s.charAt(i)<='9' && s.charAt(i)>='0'){
                     k*=10;
                     k+=s.charAt(i)-'0';
@@ -80,20 +75,18 @@ public class DecodeString {
                 int start=i+1;
                 while(i<s.length()){
                     c=s.charAt(i);
-                    if(c=='[') cnt++;
+                    if(c=='[') cnt++;   // make sure we get until the inner of '['
                     else if(c==']') cnt--;
                     if(cnt==0) break;
                     i++;
                 }
-                String str=helper(s,start,i-1);
-                for(int j=0;j<k;j++){
-                    sb.append(str);
-                }
-            }else{
+                String str=helper(s,start,i-1); // then we get the char between inner of []
+                sb.append(str.repeat(Math.max(0, k))); // repeat it k times
+            } else{
                 sb.append(c);
             }
         }
         return sb.toString();
     }
-
+    //  ***************** End of 2nd Method ******************
 }
