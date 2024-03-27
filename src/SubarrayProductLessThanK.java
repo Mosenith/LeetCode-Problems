a2a61cefea870a8f74dd6ee4408b2e39adb33411
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-27 04:58:14 -0500

    Prob 713: 2nd Approach

--- src/SubarrayProductLessThanK.java
+++ src/SubarrayProductLessThanK.java
@@ -3,13 +3,13 @@ public class SubarrayProductLessThanK {
         int[] nums = {1,1,1};
         int k = 100;
 
-        System.out.println(numSubarrayProductLessThanK(nums, 1));
+        System.out.println(numSubarrayProductLessThanK2(nums, 2));
     }
 
     // ***************** 1st Method ******************
     // Approach 1: Nested loop to check subArray
-    // Runtime  : 0ms         -> + 100.00%
-    // Memory   : 41.16MB     -> + 72.08%
+    // Runtime  : 851ms       -> + 16.13%
+    // Memory   : 47.90MB     -> + 15.67%
     public static int numSubarrayProductLessThanK(int[] nums, int k) {
         int cnt = 0;
 
@@ -28,4 +28,36 @@ public class SubarrayProductLessThanK {
     }
     //  ***************** End of 1st Method ******************
 
+    public static int numSubarrayProductLessThanK2(int[] nums, int k) {
+        int cnt = 0;
+        int maxProduct = 1;
+        for(int n : nums) {
+            maxProduct *= n;
+        }
+        System.out.println(maxProduct);
+
+        int curProduct = 1;
+        int len = nums.length-1;
+        for(int i=0; i+1<len && curProduct<k; i++) {
+            curProduct *= nums[i];
+            if(curProduct >= k) break;
+
+            cnt += factorial(len-i);
+            System.out.println();
+            System.out.println("*******\n");
+        }
+
+        return cnt;
+    }
+
+    private static int factorial(int i) {
+        if(i == 0 || i == 1) return 1;
+
+        return i * factorial(i-1);
+    }
 }
+
+// 600 : 100
+// 600 / 10 = 60 => 0-3 = 3! = 6
+// 600 / 50 = 12 => 2! = 2
+// 600 / 100=k => x
