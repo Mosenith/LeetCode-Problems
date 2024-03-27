ccb25a1ec1afef6327e8f44d9cea16c926176259
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-27 00:01:16 -0500

    Prob 856: base

--- src/ScoreOfParentheses.java
+++ src/ScoreOfParentheses.java
@@ -13,34 +13,31 @@ public class ScoreOfParentheses {
     }
 
     // ***************** 1st Method ******************
-    // Approach 1:
-    // Runtime  : 2ms         -> + 99.14%
-    // Memory   : 45.15MB     -> + 93.26%
+    // Approach 1: Init ans=0, d=0 and start from i=0, if s.at(i) = '(' -> d++
+    // Else --d and check if s.at(i-1) = '(', ans += (1<<d)
+    // Runtime  : 0ms         -> + 100.00%
+    // Memory   : 41.16MB     -> + 72.08%
     public static int scoreOfParentheses(String s) {
-        int score = 0; // since it balanced parentheses
-        for(int i=0,j=s.length()-1; i<j;) {
-            if(s.charAt(i) == '(' && s.charAt(i+1) != ')'
-                    && s.charAt(j) == ')' && s.charAt(j-1) != '(') {
-                System.out.println("2A rule");
-                // (A) rule => 2*A
-                score += ((j-i-1));
-                break;
-            } else if(s.charAt(i) == '(' && s.charAt(i+1) == ')'){
-                System.out.println("Form at the front");
-                score++;
-                i += 2;
-            } else if(s.charAt(j) == ')' && s.charAt(j-1) == '(') {
-                System.out.println("Form at the back");
-                score++;
-                j -= 2;
+        int ans = 0, d = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                ++d;
+            } else {
+                --d;
+                if (s.charAt(i - 1) == '(') {
+                    ans += 1 << d; // determine the score associated with a certain depth d of parentheses
+                }
             }
-            System.out.println(i + " : " + j);
-            System.out.println("******** " + score + " *********\n");
         }
-        return score;
+        return ans;
     }
+    //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Similar approach to the 1st
+    // First find freq(cnt[]) of tasks & maxFreq
+    // Find #maxFreq of char in cnt[] & find interval by max(tasks.length, (maxFreq-1)*(n+1)+s)
+    // Runtime  : 3ms         -> + 78.07%
+    // Memory   : 45.61MB     -> + 58.12%
+
 }
-// 1-6 = 6-1+1
-// "()" has score 1.
-// AB has score A + B, where A and B are balanced parentheses strings.
-// (A) has score 2 * A, where A is a balanced parentheses string.
