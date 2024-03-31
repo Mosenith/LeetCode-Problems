f468835b40bf02999b6f49be77a3bcf3e283b455
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-31 00:15:49 -0500

    Prob 2444: 2nd approach

--- src/CountSubarraysWithFixedBounds.java
+++ src/CountSubarraysWithFixedBounds.java
@@ -5,10 +5,10 @@ import java.util.Queue;
 
 public class CountSubarraysWithFixedBounds {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1}; // 2 => [1,3,5] & [1,3,5,2]
+        int[] nums = {1,3,5,2,7,5}; // 2 => [1,3,5] & [1,3,5,2]
         int minK = 1, maxK = 1;
 
-        System.out.println(countSubarrays(nums,minK,maxK));
+        System.out.println(countSubarrays2(nums,minK,5));
     }
 
     // ***************** 1st Method ******************
@@ -37,8 +37,25 @@ public class CountSubarraysWithFixedBounds {
     //  ***************** End of 1st Method ******************
 
     // ***************** 2nd Method ******************
-    // Approach 2: Use stack when encounter '(', push score to stack and reset score
-    // Otherwise, set score = stack.pop() + max(2*score, 1)
-    // Runtime  : 3ms         -> + 78.07%
-    // Memory   : 45.61MB     -> + 58.12%
+    // Approach 2: Similar approach to the 1st just use while and init right(i) = 0
+    // Runtime  : 5ms         -> + 100.00%
+    // Memory   : 59.44MB     -> + 83.03%
+    public static long countSubarrays2(int[] nums, int minK, int maxK) {
+        int minI = -1, maxI = -1, left = -1, right = 0;
+        long count = 0;
+        while(right < nums.length){
+            // out of range min & max
+            if(nums[right] < minK || nums[right] > maxK){
+                minI = right;
+                maxI = right;
+                left = right;
+            }
+            minI = nums[right] == minK ? right : minI;
+            maxI = nums[right] == maxK ?  right : maxI;
+            count += Math.min(minI, maxI) - left;
+            right++;
+        }
+        return count;
+    }
+    //  ***************** End of 2nd Method ******************
 }
