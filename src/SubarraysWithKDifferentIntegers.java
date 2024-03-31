b162300122187eb7d4a20bb46e5c230bda9a856d
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-03-31 00:53:44 -0500

    prob 992: base

---
+++ src/SubarraysWithKDifferentIntegers.java
@@ -0,0 +1,9 @@
+public class SubarraysWithKDifferentIntegers {
+    public static void main(String[] args) {
+
+    }
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return 0;
+    }
+}
