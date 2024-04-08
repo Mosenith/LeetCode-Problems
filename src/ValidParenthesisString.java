c52c240203bf0e71b461729eb7d23d03d4547276
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-04-07 19:48:59 -0500

    Prob 678: 2nd approach

--- src/ValidParenthesisString.java
+++ src/ValidParenthesisString.java
@@ -4,7 +4,7 @@ public class ValidParenthesisString {
     public static void main(String[] args) {
         String s = "(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())";
 
-        System.out.println(checkValidString("(*))"));
+        System.out.println(checkValidString2(s));
     }
 
     // ***************** 1st Method ******************
@@ -39,10 +39,46 @@ public class ValidParenthesisString {
         return lowerBound == 0;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Use 2 stack of integer to store leftStack -> '(' & starStack -> '*'
+    // When encounter ')', if !leftStack.empty(), pop it. If it is, pop starStack if it's not empty
+    // If both stacks are empty, return false
+    // Out of loop, if !leftStack.empty(), closing it with starStack.
+    // Star elements need to be bigger than '(' in order to close it. Otherwise, return false.
+    // Runtime  : 1ms         -> + 38.32%
+    // Memory   : 41.30MB     -> + 57.16%
+    public static boolean checkValidString2(String s) {
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftStack.push(i);
+            } else if (c == '*') {
+                starStack.push(i);
+            } else {
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!starStack.isEmpty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        // if there's still some leftover (, then complete it with *
+        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
+            // for * to be ), need to be at pos > '('
+            if (leftStack.peek() < starStack.peek()) {
+                leftStack.pop();
+                starStack.pop();
+            } else {
+                break;
+            }
+        }
+        return leftStack.isEmpty();
+    }
+    //  ***************** End of 2nd Method ******************
 }
 
-
-//        stkRight.size()+stkStar.size() == stkLeft.size() ||
-//        stkLeft.size()+stkStar.size() == stkRight.size()
-
-// 49
