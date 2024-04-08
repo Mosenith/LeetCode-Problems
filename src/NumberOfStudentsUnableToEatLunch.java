01f2980953efdbfd5eb1655343f2ec2b0e6a6d87
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-04-08 00:47:14 -0500

    Prob 1700: 1st approach

--- src/NumberOfStudentsUnableToEatLunch.java
+++ src/NumberOfStudentsUnableToEatLunch.java
@@ -13,7 +13,7 @@ public class NumberOfStudentsUnableToEatLunch {
     // Approach 1: Read students[] to queue & loop through sandwiches
     // Loop through queue till sandwiches[i]=queue.peek(), pop different element and add to the tail
     // Also keep track of the total iteration (queue.size)
-    // So if student =[1,1,1] & sandwich[0,...], after looping 3 times, it'll back to the start
+    // So if student =[1,1,1] & sandwich[0,...], after looping 3 times, it'll come back to the start
     // So if #round = queue.size, just return queue.size() -> students unable to eat
     // Runtime  : 1ms         -> + 71.05%
     // Memory   : 40.96MB     -> + 95.65%
