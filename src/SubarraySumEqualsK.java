3969ad21bb916778eab5aa72e6f46a9c3b79f1ed
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-04-09 00:45:29 -0500

    Pro 560: 1st approach

--- src/SubarraySumEqualsK.java
+++ src/SubarraySumEqualsK.java
@@ -3,27 +3,31 @@ import java.util.Map;
 
 public class SubarraySumEqualsK {
     public static void main(String[] args) {
-        int[] nums = {1,2,1,2,1};
-        int k = 3;
+        int[] nums = {1,-1,0}; // 4
+        int k = 0;
 
         System.out.println(subarraySum(nums, k));
     }
+
+    // ***************** 1st Method ******************
+    // Approach 1: Brute force - nested loop
+    // Runtime  : 1117ms      -> + 9.64%
+    // Memory   : 46.31MB     -> + 54.20%
     public static int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap();
         int count = 0;
 
         for(int i=0; i<nums.length; i++) {
-            int curKey = k - nums[i];
-            if(!map.containsKey(nums[i])) {
-                map.put(curKey,i);
-                count++;
-            } else {
-                break;
+            int curSum = 0;
+            int nextIndex = i;
+            while(nextIndex<nums.length) {
+                curSum += nums[nextIndex];
+                if(curSum == k) {
+                    count++;
+                }
+                nextIndex++;
             }
-
-            System.out.println(map);
         }
 
-        return count+1;
+        return count;
     }
 }
