d34c8efae966313ec0dd9f51ca3264d0cf809e89
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-04-14 03:54:00 -0500

    Prob 2073: 2nd approach

--- src/TimeNeededToBuyTickets.java
+++ src/TimeNeededToBuyTickets.java
@@ -1,9 +1,9 @@
 public class TimeNeededToBuyTickets {
     public static void main(String[] args) {
-        int[] tickets = {2,3,2};
-        int k = 2;
+        int[] tickets = {5,1,1,1};
+        int k = 0;
 
-        System.out.println(timeRequiredToBuy(tickets,k));
+        System.out.println(timeRequiredToBuy2(tickets,k));
     }
 
     // ***************** 1st Method ******************
@@ -29,5 +29,23 @@ public class TimeNeededToBuyTickets {
         return time;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Check i & k, if i<=k, ans+=min(t[i],t[k])
+    // Otherwise, ans += min(t[k]-1, t[i])
+    // Runtime  : 0ms      -> + 100.00%
+    // Memory   : 41.64MB  -> + 14.23%
+    public static int timeRequiredToBuy2(int[] tickets, int k) {
+        int ans = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                ans += Math.min(tickets[k], tickets[i]);
+            } else {
+                ans += Math.min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        return ans;
+    }
+    //  ***************** End of 2nd Method ******************
 
 }
