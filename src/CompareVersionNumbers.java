e0915bdc27468afb1692c806f616d5bdd32d3f01
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-03 11:38:05 -0500

    Prob 165: 1st approach

--- src/CompareVersionNumbers.java
+++ src/CompareVersionNumbers.java
@@ -1,25 +1,37 @@
+import java.util.Arrays;
+
 public class CompareVersionNumbers {
     public static void main(String[] args) {
-        String version1 = "1.0", version2 = "1.0.0";
+        String version1 = "1.1", version2 = "1.10";
 
         System.out.println(compareVersion(version1,version2));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Compare from left to right and stop on '.'
+    // If by then v1=v2, continue to compare till the '.'
+    // Runtime  : 0ms       -> + 100.00%
+    // Memory   : 41.80MB   -> + 8.85%
     public static int compareVersion(String version1, String version2) {
-        char[] v1 = version1.toCharArray();
-        char[] v2 = version2.toCharArray();
+        int l1 = version1.length();
+        int l2 = version2.length();
 
-        for(int i=0, j=0; i<v1.length && j<v2.length; i++, j++) {
-            if(v1[i] > v2[j]) {
-                while (j<v2.length && v2[j] == 0) {
-                    j++;
-                }
-                return 1;
-            } else if(v1[i] < v2[j]) {
-                return -1;
+        for(int i=0, j=0; i<l1 || j<l2; i++,j++) {
+            int a = 0, b = 0;
+            while (i < l1 && version1.charAt(i) != '.') {
+                a = a * 10 + (version1.charAt(i++) - '0');
+            }
+
+            while (j < l2 && version2.charAt(j) != '.') {
+                b = b * 10 + (version2.charAt(j++) - '0');
+            }
+            if (a != b) {
+                return a < b ? -1 : 1;
             }
         }
 
         return 0;
     }
+    //  ***************** End of 1st Method ******************
+
 }
