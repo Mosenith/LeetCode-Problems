b1e0fbdb65d0d0e5c98a39483e5a265e0e99d08e
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-04 23:20:40 -0500

    Prob 881: 1st approach

--- src/BoatsToSavePeople.java
+++ src/BoatsToSavePeople.java
@@ -1,9 +1,34 @@
+import java.util.Arrays;
+
 public class BoatsToSavePeople {
     public static void main(String[] args) {
+        int[] people = {1,5,3,5}; // 2
+        int limit = 7;
 
+        System.out.println(numRescueBoats(people,limit));
     }
 
-    public int numRescueBoats(int[] people, int limit) {
-        return 0;
+    // ***************** 1st Method ******************
+    // Approach 1: Sort array and use 2 pointers
+    // Check if sum of the left & right <= limit, boat++ & i++, j--
+    // Else boat++, only decrement the right j--
+    // Runtime  : 16ms      -> + 97.12%
+    // Memory   : 53.40MB   -> + 73.47%
+    public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boat = 0;
+
+        for(int i=0,j=people.length-1; i<=j;) {
+            if(people[i] + people[j] <= limit ) {
+                boat++;
+                i++; j--;
+            } else {
+                boat++;
+                j--;
+            }
+        }
+
+        return boat;
     }
+    //  ***************** End of 1st Method ******************
 }
