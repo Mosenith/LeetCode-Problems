43de88b6f335308f746a79f358d3009731ee5a5b
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-10 02:15:20 -0500

    prob 786: base

---
+++ src/KSmallestPrimeFraction.java
@@ -0,0 +1,11 @@
+public class KSmallestPrimeFraction {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5};
+        int k = 3;
+
+
+    }
+
+    public static int[] kthSmallestPrimeFraction(int[] arr, int k) {
+    }
+}
