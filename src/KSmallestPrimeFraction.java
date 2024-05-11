48d553c9160a3aec820660482a18df5606b7d4b0
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-11 17:33:07 -0500

    Prob 786: 2nd Approach

--- src/KSmallestPrimeFraction.java
+++ src/KSmallestPrimeFraction.java
@@ -5,7 +5,9 @@ public class KSmallestPrimeFraction {
         int[] arr = {1,2,3,5};
         int k = 3;
 
-        System.out.println(Arrays.toString(kthSmallestPrimeFraction(arr,k)));
+        System.out.println(Arrays.toString(kthSmallestPrimeFraction2(arr,k)));
+//        System.out.println(Arrays.toString(kthSmallestPrimeFraction3(arr,k)));
+
     }
 
     // ***************** 1st Method ******************
@@ -31,4 +33,54 @@ public class KSmallestPrimeFraction {
     }
     // ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Use binary search to find kth smallest fraction
+    // Use two pointers to find maxFractionLessThanMid
+    // Within the loop to find maxFractionLessThanMid, update total
+    // If total == k, return the fraction
+    // If total > k, update high = mid
+    // If total < k, update low = mid
+    // Runtime  : 3ms      -> + 96.47%
+    // Memory   : 42.22MB  -> + 78.59%
+    public static int[] kthSmallestPrimeFraction2(int[] arr, int k) {
+        int[] res = new int[2];
+        int len = arr.length;
+        double low = 0;
+        double high = 1.0;
+
+        while(low<=high) {
+            double mid = low + (high-low)/2;
+            // maxFranction <= mid
+            double maxFractionLessThanMid = 0.0;
+            int total = 0;
+            int num = 0, den = 0;
+            int j=1;
+
+            for(int i=0; i<len; i++) {
+                while(j<len && arr[i]>=arr[j]*mid) {
+                    ++j;
+                }
+                total += len-j;
+                // update maxFractionLessThanMid if fraction > maxFractionLessThanMid
+                if(j<len && maxFractionLessThanMid < arr[i]*1.0/arr[j]) {
+                    maxFractionLessThanMid = arr[i]*1.0/arr[j];
+                    num = i;
+                    den = j;
+                }
+            }
+            if(total == k) {
+                res[0] = arr[num];
+                res[1] = arr[den];
+                break;
+            }
+            if(total > k) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return res;
+    }
+    // ***************** End of 2nd Method ******************
 }
