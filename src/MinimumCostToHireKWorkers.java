b722269761b57013c7feb4c02afc734b19eb44fe
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-11 17:46:48 -0500

    Prob 857: base

---
+++ src/MinimumCostToHireKWorkers.java
@@ -0,0 +1,10 @@
+public class MinimumCostToHireKWorkers {
+    public static void main(String[] args) {
+
+    }
+
+    public static double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+
+        return 0.0;
+    }
+}
