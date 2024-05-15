106112954d61c9dadc489c4ca367655344feb2da
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-05-15 02:10:33 -0500

    prob 2812: base

---
+++ src/FindTheSafestPathInAGrid.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class FindTheSafestPathInAGrid {
+    public static void main(String[] args) {
+
+    }
+
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        return 0;
+    }
+}
