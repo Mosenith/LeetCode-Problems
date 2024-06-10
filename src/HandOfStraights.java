02d42c6a4ac680c8ee2988ff9bcd8421890f2431
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-10 00:50:21 -0500

    Prob 846: base

---
+++ src/HandOfStraights.java
@@ -0,0 +1,12 @@
+public class HandOfStraights {
+    public static void main(String[] args) {
+        int[] hand = {1,2,3,6,2,3,4,7,8};
+        int groupSize = 3;
+
+        System.out.println(isNStraightHand(hand, groupSize));
+    }
+
+    public static boolean isNStraightHand(int[] hand, int groupSize) {
+        return true;
+    }
+}
