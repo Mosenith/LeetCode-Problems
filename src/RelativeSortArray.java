2ce7cafb5cab6d41acb2ded4d05f6384a2cd50ad
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-13 02:37:43 -0500

    Prob 1122: 2nd approach

--- src/RelativeSortArray.java
+++ src/RelativeSortArray.java
@@ -6,13 +6,13 @@ public class RelativeSortArray {
         int[] arr1 = {2,3,1,3,2,4,6,7,9,2,19};
         int[] arr2 = {2,1,4,3,9,6};
 
-        System.out.println(Arrays.toString(relativeSortArray(arr1,arr2)));
+        System.out.println(Arrays.toString(relativeSortArray2(arr1,arr2)));
     }
 
     // ***************** 1st Method ******************
-    // Approach : Start from i=0 to nums.len
-    // Inner loop, keep count of element >=i, if by some points cnt>i, break
-    // Out of inner loop if cnt=i, return cnt
+    // Approach : Using TreeMap to store the frequency of elements in arr1
+    // Traverse through arr2 and add the elements to ans based on the frequency
+    // Traverse through the remaining elements in map and add them to ans
     // Runtime  : 4ms        -> + 21.58%
     // Memory   : 41.97 MB   -> + 53.15%
     public static int[] relativeSortArray(int[] arr1, int[] arr2) {
@@ -46,4 +46,26 @@ public class RelativeSortArray {
     }
     // ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach : Using TreeMap to store the frequency of elements in arr1
+    // Traverse through arr1 and store the element and its position in arr2
+    // Sort the arr based on the position
+    // Runtime  : 3ms        -> + 42.92%
+    // Memory   : 41.97 MB   -> + 53.15%
+    public static int[] relativeSortArray2(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> pos = new HashMap<>(arr2.length);
+        for (int i = 0; i < arr2.length; ++i) {
+            pos.put(arr2[i], i);
+        }
+        int[][] arr = new int[arr1.length][0];
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = new int[] {arr1[i], pos.getOrDefault(arr1[i], arr2.length + arr1[i])};
+        }
+        Arrays.sort(arr, (a, b) -> a[1] - b[1]);
+        for (int i = 0; i < arr.length; ++i) {
+            arr1[i] = arr[i][0];
+        }
+        return arr1;
+    }
+    // ***************** End of 2nd Method ******************
 }
