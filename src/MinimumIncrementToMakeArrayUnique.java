0789ab188357ab63aea93d2d12b83805c68c1ee6
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-15 14:57:23 -0500

    Prob 2037: 3rd approach

--- src/MinimumIncrementToMakeArrayUnique.java
+++ src/MinimumIncrementToMakeArrayUnique.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class MinimumIncrementToMakeArrayUnique {
     public static void main(String[] args) {
-        int[] nums = {1,2,2};
+        int[] nums = {3,2,1,2,1,7};
 
-        System.out.println(minIncrementForUnique(nums));
+        System.out.println(minIncrementForUnique3(nums));
     }
 
     // ***************** 1st Method ******************
@@ -25,4 +25,42 @@ public class MinimumIncrementToMakeArrayUnique {
         }
         return moves;
     }
+    // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach : Optimized the 1st approach
+    // Runtime  : 552ms        -> + 7.54%
+    // Memory   : 59.98 MB    -> + 64.26%
+    public static int minIncrementForUnique2(int[] nums) {
+        int moves = 0;
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++) {
+            int prev = i-1;
+            while(nums[i] <= nums[prev]) {
+                nums[i]++;
+                moves++;
+            }
+        }
+        return moves;
+    }
+    // ***************** End of 2nd Method ******************
+
+    // ***************** 3rd Method ******************
+    // Approach : Optimized the 1st & 2nd approach
+    // Runtime  : 38ms        -> + 43.61%
+    // Memory   : 59.99 MB    -> + 64.26%
+    public static int minIncrementForUnique3(int[] nums) {
+        int moves = 0;
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++) {
+            int prev = i-1;
+            if(nums[i] <= nums[prev]) {
+                moves += Math.abs(nums[i] - nums[prev]) + 1;
+                nums[i] = nums[prev] + 1;
+            }
+        }
+        return moves;
+    }
+    // ***************** End of 2nd Method ******************
+
 }
