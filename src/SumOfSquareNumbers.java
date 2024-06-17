82a789f5b2391eca27ab71083f66ea1d29164d48
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-17 02:43:21 -0500

    Prob 633: update runtime & memory

--- src/SumOfSquareNumbers.java
+++ src/SumOfSquareNumbers.java
@@ -7,8 +7,8 @@ public class SumOfSquareNumbers {
     // Approach 1: Use two pointers i and r
     // Start from 0 and r = sqrt(c)
     // If i^2 + r^2 > c, r--. If i^2 + r^2 < c, i++. Else, return true
-    // Runtime  : 0ms       -> + 100.00%
-    // Memory   : 43.55MB   -> + 95.47%
+    // Runtime  : 3ms       -> + 97.79%
+    // Memory   : 40.74MB   -> + 9.53%
     public static boolean judgeSquareSum(int c) {
         if(c == 1) return true;
         long r = (int) Math.sqrt(c);
