8610ba2c58842e672087e48baba2e56c69f38f39
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-20 02:56:38 -0500

    Prob 1552: 2nd approach

--- src/MagneticForceBetweenTwoBalls.java
+++ src/MagneticForceBetweenTwoBalls.java
@@ -47,4 +47,35 @@ public class MagneticForceBetweenTwoBalls {
     }
     //  ***************** End of 1st Method ******************
 
+    // ***************** 2nd Method ******************
+    // Approach 2: Optimized the 1st method by using >>> 1 instead of / 2
+    // Runtime  : 41ms      -> + 81.65%
+    // Memory   : 55.56MB   -> + 78.33%
+    public static int maxDistance2(int[] position, int m) {
+        Arrays.sort(position);
+        int left = 1, right = position[position.length - 1];
+        while (left < right) {
+            int mid = (left + right + 1) >>> 1;
+            if (check(position, mid, m)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean check(int[] position, int f, int m) {
+        int prev = position[0];
+        int cnt = 1;
+        for (int i = 1; i < position.length; ++i) {
+            int curr = position[i];
+            if (curr - prev >= f) {
+                prev = curr;
+                ++cnt;
+            }
+        }
+        return cnt >= m;
+    }
+    //  ***************** End of 2nd Method ******************
 }
