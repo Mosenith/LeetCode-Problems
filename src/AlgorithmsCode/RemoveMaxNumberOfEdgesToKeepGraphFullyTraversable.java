26be83ca1428aeb9e96279cb41d60c3ad48395b3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-06-30 01:05:51 -0500

    Prob 1579: Base

---
+++ src/AlgorithmsCode/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
@@ -0,0 +1,13 @@
+package AlgorithmsCode;
+
+public class RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] edges = {{3,1,2},{3,2,3},{1,1,3},{1,2,4},{1,1,2},{2,3,4}};
+    }
+
+    public static int maxNumEdgesToRemove(int n, int[][] edges) {
+
+        return 0;
+    }
+}
