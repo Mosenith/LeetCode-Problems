458901ad5de71b73f6e21e854342a0e50ff85591
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-03 16:13:39 -0500

    Prob 1509: 1st approach

--- src/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ src/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -2,27 +2,31 @@ import java.util.Arrays;
 
 public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
     public static void main(String[] args) {
-        int[] nums = {6,6,0,1,1,4,6};
+        int[] nums = {82,81,95,75,20};
 
         System.out.println(minDifference(nums));
     }
 
+    // ***************** 1st Method ******************
+    // Approach : Sort array and init minDiff to Integer.MAX_VALUE
+    // Then, find the minimum by leaving out the 3 smallest
+    // Then, find the minimum by leaving out the 3 largest
+    // Then, find the minimum by leaving out the 2 smallest and 1 largest
+    // Then, find the minimum by leaving out the 1 smallest and 2 largest
+    // Runtime  : 15ms        -> + 96.33%
+    // Memory   : 58.04MB     -> + 5.13%
     public static int minDifference(int[] nums) {
         if(nums.length <= 4) return 0;
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-
-        int min = nums[0];
         int len = nums.length;
+        int minDiff = Integer.MAX_VALUE;
 
-        for(int i=nums.length-1; i>=len-3; i--) {
-            nums[i] = min;
-            System.out.println("******\n");
-        }
+        minDiff = Math.min(minDiff,nums[len-1]-nums[3]);
+        minDiff = Math.min(minDiff,nums[len-4]-nums[0]);
+        minDiff = Math.min(minDiff,nums[len-2]-nums[2]);
+        minDiff = Math.min(minDiff,nums[len-3]-nums[1]);
 
-        System.out.println(Arrays.toString(nums));
-        Arrays.sort(nums);
-
-        return nums[len-1]-nums[0];
+        return minDiff;
     }
+    // ***************** End of 1st Method ******************
 }
