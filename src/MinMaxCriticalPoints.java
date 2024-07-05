e1d84ad4283fe744454da6820f5d0b97770ae6cb
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-05 15:37:05 -0500

    Prob 2058: 2nd approach

--- src/MinMaxCriticalPoints.java
+++ src/MinMaxCriticalPoints.java
@@ -41,7 +41,7 @@ public class MinMaxCriticalPoints {
         head.next.next.next.next.next.next.next = new ListNode(2);
         head.next.next.next.next.next.next.next.next = new ListNode(7);
 
-        System.out.println(Arrays.toString(nodesBetweenCriticalPoints(head)));
+        System.out.println(Arrays.toString(nodesBetweenCriticalPoints2(head)));
     }
 
     // ***************** 1st Method ******************
@@ -82,4 +82,40 @@ public class MinMaxCriticalPoints {
         }
         return new int[]{min,max};
     }
+    //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Optimized above approach by using integer min & max instead of List
+    // Update min when critical points is met && startPos != -1
+    // Each iteration update curPos to pos, prev to cur, cur to next, pos++
+    // Out of loop, check if startPos == -1 or min == Integer.MAX_VALUE, return [-1,-1]
+    // Otherwise, calculate max and return [min,max]
+    // Runtime  : 4ms      -> + 100.00%
+    // Memory   : 57.26MB  -> + 85.06%
+    public static int[] nodesBetweenCriticalPoints2(ListNode head) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int startPos = -1, curPos = -1;
+        int pos = 0;
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while(cur.next != null) {
+            if((prev.val < cur.val && cur.val > cur.next.val) || (prev.val > cur.val && cur.val < cur.next.val)) {
+                if(startPos == -1) {
+                    startPos = pos;
+                } else {
+                    min = Math.min(min, pos-curPos);
+                }
+                curPos = pos;
+            }
+            prev = cur;
+            cur = cur.next;
+            pos++;
+        }
+
+        if(startPos == -1 || min == Integer.MAX_VALUE) return new int[]{-1,-1};
+        max = Math.max(max, curPos-startPos);
+        return new int[]{min,max};
+    }
+    //  ***************** End of 2nd Method ******************
 }
