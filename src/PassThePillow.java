fd05fd5731d818c1f92758e71f946fd443b8b3c5
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-06 01:20:00 -0500

    Prob 2582: base

---
+++ src/PassThePillow.java
@@ -0,0 +1,17 @@
+public class PassThePillow {
+    public static void main(String[] args) {
+        int n=33, time=218;
+
+        System.out.println(passThePillow(2,341));
+
+    }
+
+
+
+    // 33*6=198
+    // 218-198=20
+    // from 1 move 26 steps => 27
+    public static int passThePillow(int n, int time) {
+        return 0;
+    }
+}
