4332c90b785bef5d843156b39c186cccdda1b435
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-08 18:05:20 -0500

    Prob 1823: 2nd approach

--- src/FindTheWinnerOfTheCircularGame.java
+++ src/FindTheWinnerOfTheCircularGame.java
@@ -1,9 +1,14 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class FindTheWinnerOfTheCircularGame {
     public static void main(String[] args) {
         System.out.println(findTheWinner(6,5));
+        System.out.println(findTheWinner2(6,5));
+        System.out.println(findTheWinner3(6,5));
+
     }
 
     // ***************** 1st Method ******************
@@ -41,4 +46,52 @@ public class FindTheWinnerOfTheCircularGame {
         return ls.get(0);
     }
     // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Using Josephus Problem Iterative Formula
+    // Runtime  : 0ms      -> + 100.00%
+    // Memory   : 40.34MB  -> + 65.98%
+    public static int findTheWinner2(int n, int k) {
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            ans = (ans + k) % i;
+        }
+        return ans+1;
+    }
+    // ***************** End of 2nd Method ******************
+
+    // ***************** 3rd Method ******************
+    // Approach 3: Using queue to store all numbers from 1 to n
+    // Until queue.size() = 1, store the removed element before reaching k steps in tmpQueue,
+    // Then remove element at k steps, and add all the elements in tmpQueue back to queue
+    // If by any chance queue is empty before reaching k steps, add all elements in tmpQueue back to queue
+    // Proceed until queue.size() = 1 and return the element
+    // Runtime  : 84ms     -> + 5.02%
+    // Memory   : 44.20MB  -> + 18.28%
+    public static int findTheWinner3(int n, int k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i=1; i<=n; i++) {
+            queue.offer(i);
+        }
+
+        while(queue.size() > 1) {
+            Queue<Integer> tmp = new ArrayDeque<>();
+            for(int i= 1; i<k; i++) {
+                int v = queue.remove();
+                tmp.offer(v);
+                if(queue.isEmpty()) {
+                    while(!tmp.isEmpty()) {
+                        queue.offer(tmp.remove());
+                    }
+                }
+            }
+            queue.remove();
+            while(!tmp.isEmpty()) {
+                queue.offer(tmp.remove());
+            }
+        }
+
+        return queue.peek();
+    }
+    // ***************** End of 3rd Method ******************
 }
