cb211f2e49f4c4ad415b76edbcf642fa4f23b00f
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-11 23:58:46 -0500

    Prob 1190: 1st approach

--- src/ReverseSubstringsBetweenEachPairParentheses.java
+++ src/ReverseSubstringsBetweenEachPairParentheses.java
@@ -1,60 +1,43 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class ReverseSubstringsBetweenEachPairParentheses {
     public static void main(String[] args) {
         String s = "(ed(et(oc))el)";
 
-        System.out.println(reverseParentheses("sxmdll(q)eki(x)"));
+        System.out.println(reverseParentheses(s));
     }
 
+    // ***************** 1st Method ******************
+    // Approach 1: Use stack to store the index of '('
+    // Loop through s, if s[i] == '(', push i to stack
+    // If s[i] == ')', pop from stack and assign d[i] = tmp and d[tmp] = i
+    // Loop through s, if s[i] == '(' or ')', i = d[i] and x = -x
+    // Else, append s[i] to sb. Return sb.toString()
+    // Runtime  : 1ms         -> + 62.26%
+    // Memory   : 41.62MB     -> + 55.05%
     public static String reverseParentheses(String s) {
         StringBuilder sb = new StringBuilder();
-        Queue<String> leftQueue = new ArrayDeque<>();
-        Stack<String> rightStack = new Stack<>();
-        int changes = 0;
+        int[] d = new int[s.length()];
+        Deque<Integer> stk = new ArrayDeque<>();
 
-        for(int i=0,j=s.length()-1; i<=j; i++,j--) {
-            StringBuilder leftTmp = new StringBuilder();
-            StringBuilder rightTmp = new StringBuilder();
-
+        for(int i=0; i<s.length(); ++i) {
             if(s.charAt(i) == '(') {
-                i++;
-                while(i<j && s.charAt(i) != '(' && s.charAt(i) != ')') {
-                    leftTmp.append(s.charAt(i++));
-                }
-
-                if(s.charAt(i) != ')') {
-                    while(s.charAt(j) != ')') {
-                        rightStack.add(String.valueOf(s.charAt(j--)));
-                    }
-                }
-            } else {
-                while(s.charAt(i) != '(') {
-                    sb.append(s.charAt(i));
-                }
+                stk.push(i);
+            } else if(s.charAt(i) == ')') {
+                int tmp = stk.pop();
+                d[i] = tmp;
+                d[tmp] = i;
             }
-
-            // changes is even reverse, otherwise no reverse
-            if(changes % 2 == 0) {
-                if(!rightTmp.isEmpty()) {
-                    sb.append(rightTmp);
-                }
-                if(!leftTmp.isEmpty()) {
-                    rightStack.add(leftTmp.reverse().toString());
-                }
-            } else {
-                if(!leftTmp.isEmpty())
-                    sb.append(leftTmp);
-                if(!rightTmp.isEmpty())
-                    rightStack.add(rightTmp.reverse().toString());
-            }
-            changes++;
         }
 
-        while(!rightStack.isEmpty()) {
-            sb.append(rightStack.pop());
+        int i=0, x=1;
+        while(i < s.length()) {
+            if(s.charAt(i) == '(' || s.charAt(i) == ')') {
+                i = d[i];
+                x = -x;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
 
         return sb.toString();
