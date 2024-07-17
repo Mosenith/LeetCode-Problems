6d87770d77674eb6c2a3b6a11104b429d0ba2871
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-17 16:24:00 -0500

    Prob 1110: C# 2nd approach

--- CSharp-Solutions/LeetCodeCSharp/LeetCodeCSharp/DeleteNodesAndReturnForest.cs
+++ CSharp-Solutions/LeetCodeCSharp/LeetCodeCSharp/DeleteNodesAndReturnForest.cs
@@ -37,17 +37,18 @@ namespace LeetCodeCSharp
         // If it is not, return the current node
         // Runtime  : 129ms     -> + 14.29%
         // Memory   : 50.86MB   -> + 18.68%
-        public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete) {
+        public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
+        {
             IList<TreeNode> ans = new List<TreeNode>();
             HashSet<int> set = new HashSet<int>();
 
-            foreach(int i in to_delete)
+            foreach (int i in to_delete)
             {
                 set.Add(i);
             }
 
             dfsDelete(root, ans, set);
-            if(!set.Contains(root.val))
+            if (!set.Contains(root.val))
                 ans.Add(root);
 
             return ans;
@@ -55,17 +56,17 @@ namespace LeetCodeCSharp
 
         private TreeNode dfsDelete(TreeNode root, IList<TreeNode> ans, HashSet<int> set)
         {
-            if(root == null)
+            if (root == null)
                 return null;
 
             root.left = dfsDelete(root.left, ans, set);
             root.right = dfsDelete(root.right, ans, set);
 
-            if(set.Contains(root.val))
+            if (set.Contains(root.val))
             {
-                if(root.left != null)
+                if (root.left != null)
                     ans.Add(root.left);
-                if(root.right != null)
+                if (root.right != null)
                     ans.Add(root.right);
                 return null;
             }
@@ -73,5 +74,66 @@ namespace LeetCodeCSharp
             return root;
         }
         // ***************** End of 1st Method ******************
+
+        // ***************** 2nd Method ******************
+        // Approach 2: Init HashSet to store to_delete values and List to store the result
+        // Check if the root is not in the set, add it to the result list
+        // Traverse the tree and check if the current node is in the set
+        // If it is, add the left(if left!=null) and right(if right!=null) node to the ans list and return null
+        // If it is not, return the current node
+        // Runtime  : 113ms     -> + 76.92%
+        // Memory   : 50.64MB   -> + 25.27%
+        public IList<TreeNode> result = new List<TreeNode>();
+        public IList<TreeNode> DelNodes2(TreeNode root, int[] to_delete)
+        {
+            var d = new HashSet<int>(to_delete);
+            if (root != null && !d.Contains(root.val))
+            {
+                result.Add(root);
+            }
+
+            Traverse(null, root, true, d);
+            Traverse(null, root, false, d);
+
+            return result;
+        }
+
+        private void Traverse(TreeNode parent, TreeNode current, bool left, HashSet<int> d)
+        {
+            if (current == null)
+            {
+                return;
+            }
+
+            Traverse(current, current.left, true, d);
+            Traverse(current, current.right, false, d);
+
+            if (d.Contains(current.val))
+            {
+                if (current.left != null)
+                {
+                    result.Add(current.left);
+                }
+
+                if (current.right != null)
+                {
+                    result.Add(current.right);
+                }
+
+                if (parent != null)
+                {
+                    if (left)
+                    {
+                        parent.left = null;
+                    }
+                    else
+                    {
+                        parent.right = null;
+                    }
+                }
+                d.Remove(current.val);
+            }
+        }
+        // ***************** End of 2nd Method ******************
     }
 }
