b7f07c0b6e5e08b6a3acd3388e7b56ee20589755
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-07-30 14:15:26 -0500

    Prob 1653: 2nd approach

--- src/MinimumDeletionsToMakeStringBalanced.java
+++ src/MinimumDeletionsToMakeStringBalanced.java
@@ -5,6 +5,7 @@ public class MinimumDeletionsToMakeStringBalanced {
         String s = "baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb";
 
         System.out.println(minimumDeletions(s));
+        System.out.println(minimumDeletions2(s));
     }
 
     // ***************** 1st Method ******************
@@ -28,4 +29,27 @@ public class MinimumDeletionsToMakeStringBalanced {
         return minDelete;
     }
     //  ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Loop through each char in s
+    // If char is 'a', increment minDelete with min(minDelete+1,bCount)
+    // Else, increment bCount
+    // Runtime  : 19ms     -> + 92.48%
+    // Memory   : 45.24MB  -> + 77.16%
+    public static int minimumDeletions2(String s) {
+        int bCount = 0; // count of 'b's encountered so far
+        int minDelete = 0; // minimum deletions needed
+
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                bCount++; // increment the count of 'b's
+            } else if (c == 'a') {
+                // minDelete is either delete this 'a' or delete all 'b's before this 'a'
+                minDelete = Math.min(minDelete + 1, bCount);
+            }
+        }
+
+        return minDelete;
+    }
+    //  ***************** End of 2nd Method ******************
 }
