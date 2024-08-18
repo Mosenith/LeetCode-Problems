74b2a6698e1f076f2639828d6c342c178787e987
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-08-18 14:08:17 -0500

    Prob 264: 2nd approach

--- src/UglyNumberII.java
+++ src/UglyNumberII.java
@@ -5,6 +5,7 @@ public class UglyNumberII {
     public static void main(String[] args) {
         int n = 11;
         System.out.println(nthUglyNumber(n));
+        System.out.println(nthUglyNumber2(n));
     }
 
     // ***************** 1st Method ******************
@@ -18,7 +19,6 @@ public class UglyNumberII {
         while(cnt != n) {
             uglyNum++;
             int curNum = uglyNum;
-            System.out.println("currentNum -> " + curNum);
             while(curNum > 1) {
                 if(curNum % 2 == 0) {
                     curNum /= 2;
@@ -35,13 +35,42 @@ public class UglyNumberII {
                 cnt++;
                 ls.add(uglyNum);
             }
-            System.out.println("ls => " + ls);
-            System.out.println("****\n");
         }
         return uglyNum;
     }
     //  ***************** End of 1st Method ******************
 
-}
+    // ***************** 2nd Method ******************
+    // Approach 2: Ugly number - prime factors are limited to 2, 3, and 5
+    // Create an array to store the ugly numbers, with init uglyNumbers[0] = 1
+    // Create 3 pointers i2, i3, i5 to keep track of the next ugly number
+    // Loop from i=1 to n, get the minimum ugly number from the 3 pointers
+    // Get the minimum ugly number by Math.min(uglyNumbers[i2] * 2, uglyNumbers[i3] * 3, uglyNumbers[i5] * 5)
+    // Assign the minimum ugly number to uglyNumbers[i]
+    // If the minimum ugly number is from i2, increment i2
+    // If the minimum ugly number is from i3, increment i3
+    // If the minimum ugly number is from i5, increment i5
+    // Return the last ugly number
+    // Runtime  : 2ms         -> + 91.97%
+    // Memory   : 41.62MB     -> + 50.00%
+    public static int nthUglyNumber2(int n) {
+        if (n == 1) return 1;
 
-// 12,14
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            // get the minimum ugly number first
+            int nextUgly = Math.min(Math.min(uglyNumbers[i2] * 2, uglyNumbers[i3] * 3), uglyNumbers[i5] * 5);
+            uglyNumbers[i] = nextUgly;
+
+            if (nextUgly == uglyNumbers[i2] * 2) i2++;
+            if (nextUgly == uglyNumbers[i3] * 3) i3++;
+            if (nextUgly == uglyNumbers[i5] * 5) i5++;
+        }
+
+        return uglyNumbers[n - 1];
+    }
+    //  ***************** End of 2nd Method ******************
+}
