6dcc3e9263cf91a64b12e86ef8844573afc65987
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-08-24 00:58:29 -0500

    Prob 592: 2nd approach

--- src/FractionAdditionAndSubtraction.java
+++ src/FractionAdditionAndSubtraction.java
@@ -12,8 +12,8 @@ public class FractionAdditionAndSubtraction {
     // Check if t == 0 && b == 0, assign t = top, b = bottom
     // If not then check if b == bottom, add top to t, make irreducible fraction
     // Otherwise, add top*bottom to t, make irreducible fraction
-    // Runtime  : 7ms        -> + 53.57%
-    // Memory   : 41.86MB    -> + 59.82%
+    // Runtime  : 5ms        -> + 74.11%
+    // Memory   : 41.80MB    -> + 59.82%
     public static String fractionAddition(String expression) {
         char[] c = expression.toCharArray();
         int len = c.length;
@@ -82,5 +82,44 @@ public class FractionAdditionAndSubtraction {
         return new int[]{t,b};
     }
     // ***************** End of 1st Method ******************
+
+    // ***************** 2nd Method ******************
+    // Approach 2: Init x=0 which the top, y=6*7*8*9*10, which is the bottom of the fraction
+    // Check if the first char is digit, if yes then add '+' to the expression
+    // Loop through each char in expression then check sign
+    // Loop until the next sign, then split the fraction by '/'
+    // Calculate the top by sign*a*y/b, then move to the next sign
+    // Calculate the gcd of x and y, then have x/gcd + '/' + y/gcd
+    // Runtime  : 5ms        -> + 74.11%
+    // Memory   : 41.80MB    -> + 59.82%
+    public static String fractionAddition2(String expression) {
+        int x = 0, y = 6 * 7 * 8 * 9 * 10;
+        if (Character.isDigit(expression.charAt(0))) {
+            expression = "+" + expression;
+        }
+        int i = 0, n = expression.length();
+        while (i < n) {
+            int sign = expression.charAt(i) == '-' ? -1 : 1;
+            ++i;
+            int j = i;
+            while (j < n && expression.charAt(j) != '+' && expression.charAt(j) != '-') {
+                ++j;
+            }
+            String s = expression.substring(i, j);
+            String[] t = s.split("/");
+            int a = Integer.parseInt(t[0]), b = Integer.parseInt(t[1]);
+            x += sign * a * y / b;
+            i = j;
+        }
+        int z = gcd(Math.abs(x), y);
+        x /= z;
+        y /= z;
+        return x + "/" + y;
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    // ***************** End of 2nd Method ******************
 
 }
